e06c6de1e8787536c60dc4cb61e753e174c63795
swisswheel <swisswheel@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2005-10-27 07:40:41 +0000

    Very small header modification.

--- el4j/framework/module/security/java/ch/elca/el4j/services/security/authentication/DefaultAuthenticationService.java
+++ el4j/framework/module/security/java/ch/elca/el4j/services/security/authentication/DefaultAuthenticationService.java
@@ -11,7 +11,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
- * For alternative licensing, please contact info@elca.ch 
+ * For alternative licensing, please contact info@elca.ch
  */
 package ch.elca.el4j.services.security.authentication;
 
