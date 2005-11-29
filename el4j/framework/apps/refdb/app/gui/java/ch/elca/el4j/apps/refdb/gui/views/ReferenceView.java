5499b7713f67b0b328fcfa178bfb78212e455897
swisswheel <swisswheel@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2005-11-29 07:58:32 +0000

    Javafolder added.

---
+++ el4j/framework/apps/refdb/app/gui/java/ch/elca/el4j/apps/refdb/gui/views/ReferenceView.java
@@ -0,0 +1,50 @@
+/*
+ * EL4J, the Enterprise Library for Java, complementing Spring http://el4j.sf.net
+ * Copyright (C) 2005 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * This program is published under the GNU General Public License (GPL) license.
+ * http://www.gnu.org/licenses/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+
+package ch.elca.el4j.apps.refdb.gui.views;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+/**
+ * Reference service view.
+ *
+ * <script type="text/javascript">printFileStatus
+ *   ("$Source$",
+ *    "$Revision$",
+ *    "$Date$",
+ *    "$Author$"
+ * );</script>
+ *
+ * @author Martin Zeltner (MZE)
+ */
+public class ReferenceView extends AbstractReferenceServiceView {
+
+    /**
+     * {@inheritDoc}
+     * 
+     * Returns the root component for this view.
+     */
+    protected JComponent createControl() {
+        JPanel p = new JPanel(new BorderLayout());
+        p.setBackground(Color.RED);
+        return p;
+    }
+
+}
