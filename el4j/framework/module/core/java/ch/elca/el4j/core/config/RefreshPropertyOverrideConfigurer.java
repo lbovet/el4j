dd0018b21e21957a52e2784cb4cc1a2e8e84d3f8
swisswheel <swisswheel@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-02-06 08:51:43 +0000

    Solves issue http://opensource2.atlassian.com/projects/spring/browse/SPR-1657 for the property override configurer.
    Description:
    --------------
    I have to PropertyOverrideConfigurers. The first one has order number 0, the second 1. The first one will load the property "db.name". Now I'd like to profit in the second one (see xml snippet below). I this way I can easily change i.e. the database by adapting only property!

---
+++ el4j/framework/module/core/java/ch/elca/el4j/core/config/RefreshPropertyOverrideConfigurer.java
@@ -0,0 +1,119 @@
+/*
+ * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
+ * the spring framework, http://el4j.sf.net
+ * Copyright (C) 2006 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * EL4J is published under the GNU General Public License (GPL) Version 2.0.
+ * http://www.gnu.org/licenses/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+package ch.elca.el4j.core.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyOverrideConfigurer;
+
+import ch.elca.el4j.services.monitoring.notification.CoreNotificationHelper;
+
+/**
+ * HACK!!!
+ * 
+ * This property override configurer is used to circumvent the problem with 
+ * the abstract application context. The application context instantiates the 
+ * ordered bean factory post processor at the same time, so further loaded
+ * property override configurers don't have any influence on later loaded 
+ * property override configurers. See 
+ * <a href="http://opensource2.atlassian.com/projects/spring/browse/SPR-1657">
+ * Spring JIRA entry
+ * </a> for more details. This bean must be used as prototype and must have
+ * attribute <code>id</code> set.
+ *
+ * <script type="text/javascript">printFileStatus
+ *   ("$Source$",
+ *    "$Revision$",
+ *    "$Date$",
+ *    "$Author$"
+ * );</script>
+ *
+ * @author Martin Zeltner (MZE)
+ */
+public class RefreshPropertyOverrideConfigurer extends
+    PropertyOverrideConfigurer implements BeanNameAware {
+
+    /**
+     * Flag to mark that this instance has been created by this class or a 
+     * subclass of it.
+     */
+    private boolean m_selfInstantiated = false;
+    
+    /**
+     * Is the name of this bean.
+     */
+    private String m_beanName;
+    
+    /**
+     * {@inheritDoc}
+     * 
+     * Creates a fresh instance of this class and invokes this method of the
+     * new instance.
+     */
+    public void postProcessBeanFactory(
+        ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        if (isSelfInstantiated()) {
+            super.postProcessBeanFactory(beanFactory);
+        } else {
+            String beanName = getBeanName();
+            if (!beanFactory.containsBean(beanName)) {
+                CoreNotificationHelper.notifyMisconfiguration(
+                    "Refresh property override configurer with name '" 
+                        + beanName + "' not found!");
+            }
+            if (beanFactory.isSingleton(beanName)) {
+                CoreNotificationHelper.notifyMisconfiguration(
+                    "Refresh property override configurer with name '" 
+                        + beanName + "' must be prototype!");
+            }
+            RefreshPropertyOverrideConfigurer configurer 
+                = (RefreshPropertyOverrideConfigurer) beanFactory.getBean(
+                    beanName);
+            configurer.setSelfInstantiated(true);
+            configurer.postProcessBeanFactory(beanFactory);
+        }
+    }
+
+    /**
+     * @return Returns the selfInstantiated.
+     */
+    protected final boolean isSelfInstantiated() {
+        return m_selfInstantiated;
+    }
+
+    /**
+     * @param selfInstantiated The selfInstantiated to set.
+     */
+    protected final void setSelfInstantiated(boolean selfInstantiated) {
+        m_selfInstantiated = selfInstantiated;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public final void setBeanName(String beanName) {
+        m_beanName = beanName;
+    }
+
+    /**
+     * @return Returns the beanName.
+     */
+    public final String getBeanName() {
+        return m_beanName;
+    }
+}
