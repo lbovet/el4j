6096887569d9d1bbaa3a89496ba1eb26dc2228f6
swisswheel <swisswheel@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-02-06 09:17:27 +0000

    Not cell focusable table cell renderers added.

--- trunk/el4j/framework/module/spring_rcp/java/ch/elca/el4j/services/gui/swing/table/BooleanNoFocusTableCellRenderer.java
+++ trunk/el4j/framework/module/spring_rcp/java/ch/elca/el4j/services/gui/swing/table/BooleanNoFocusTableCellRenderer.java
@@ -1,7 +1,7 @@
 /*
  * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
  * the spring framework, http://el4j.sf.net
- * Copyright (C) 2005 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * Copyright (C) 2006 by ELCA Informatique SA, Av. de la Harpe 22-24,
  * 1000 Lausanne, Switzerland, http://www.elca.ch
  *
  * EL4J is published under the GNU General Public License (GPL) Version 2.0.
@@ -18,11 +18,13 @@ package ch.elca.el4j.services.gui.swing.table;
 
 import java.awt.Component;
 
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
 
 /**
- * Table cell renderer used for tables where only complete rows can be selected.
+ * Table cell renderer for boolean value. Cell will be displayed as check box.
  *
  * <script type="text/javascript">printFileStatus
  *   ("$Source$",
@@ -33,15 +35,20 @@ import javax.swing.table.DefaultTableCellRenderer;
  *
  * @author Martin Zeltner (MZE)
  */
-public class LineSelectionTableCellRenderer extends DefaultTableCellRenderer {
+public class BooleanNoFocusTableCellRenderer extends JCheckBox 
+    implements TableCellRenderer {
+    /**
+     * Default constructor.
+     */
+    public BooleanNoFocusTableCellRenderer() {
+        setHorizontalAlignment(JLabel.CENTER);
+    }
+
     /**
      * {@inheritDoc}
-     * 
-     * Highlight selected row. Focused cell will not be specially highlighted.
      */
-    public Component getTableCellRendererComponent(JTable table, Object value, 
+    public Component getTableCellRendererComponent(JTable table, Object value,
         boolean isSelected, boolean hasFocus, int row, int column) {
-        
         // Change colors of selected cell
         if (isSelected) {
             super.setForeground(table.getSelectionForeground());
@@ -51,15 +58,8 @@ public class LineSelectionTableCellRenderer extends DefaultTableCellRenderer {
             super.setBackground(table.getBackground());
         }
 
-        // Set table font for cell
-        setFont(table.getFont());
-
-        // Never highlight focused cell
-        setBorder(noFocusBorder);
-        
-        // Set value for cell
-        setValue(value);
-
+        // Check box if value equals true
+        setSelected((value != null && ((Boolean) value).booleanValue()));
         return this;
     }
 }
