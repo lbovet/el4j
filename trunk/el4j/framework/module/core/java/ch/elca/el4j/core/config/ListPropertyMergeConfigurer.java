f97c52c2ed91b1184a71c140ae2623df165c25b6
swisswheel <swisswheel@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-02-06 08:54:42 +0000

    Updated. Now lists can now be appended and prepended with new values.

--- trunk/el4j/framework/module/core/java/ch/elca/el4j/core/config/ListPropertyMergeConfigurer.java
+++ trunk/el4j/framework/module/core/java/ch/elca/el4j/core/config/ListPropertyMergeConfigurer.java
@@ -14,13 +14,13 @@
  *
  * For alternative licensing, please contact info@elca.ch
  */
-
 package ch.elca.el4j.core.config;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Properties;
@@ -30,13 +30,14 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.PropertyValue;
-import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.PropertyOverrideConfigurer;
 import org.springframework.core.io.Resource;
 import org.springframework.util.DefaultPropertiesPersister;
 import org.springframework.util.PropertiesPersister;
+
+import ch.elca.el4j.services.monitoring.notification.CoreNotificationHelper;
 
 /**
  * Adds an entry to a list property of a Spring bean. A file, e.g.
@@ -65,19 +66,14 @@ import org.springframework.util.PropertiesPersister;
  * </script>
  * 
  * @author Raphael Boog (RBO)
+ * @author Martin Zeltner (MZE)
  */
 public class ListPropertyMergeConfigurer extends PropertyOverrideConfigurer {
     /**
-     * Used logger.
+     * Private logger.
      */
-    private static Log s_logger = LogFactory
-            .getLog(ListPropertyMergeConfigurer.class);
-
-    /**
-     * Execution order of property override.
-     * Default is the same as non-ordered. 
-     */
-    private int m_order = Integer.MAX_VALUE;
+    private static Log s_logger 
+        = LogFactory.getLog(ListPropertyMergeConfigurer.class);
 
     /**
      * Properties.
@@ -104,111 +100,150 @@ public class ListPropertyMergeConfigurer extends PropertyOverrideConfigurer {
      * Ignore if a resource could not be found.
      */
     private boolean m_ignoreResourceNotFound = false;
+    
+    /**
+     * Marks if new items should be pre- or appended to the old values.
+     */
+    private boolean m_insertNewItemsAfter = true;
 
     /**
      * {@inheritDoc}
      */
-    public void setOrder(int order) {
-        this.m_order = order;
+    public final void setProperties(Properties properties) {
+        m_properties = properties;
     }
 
     /**
      * {@inheritDoc}
      */
-    public int getOrder() {
-        return m_order;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void setProperties(Properties properties) {
-        this.m_properties = properties;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void setLocation(Resource location) {
-        this.m_locations = new Resource[] {location};
+    public final void setLocation(Resource location) {
+        m_locations = new Resource[] {location};
         super.setLocation(location);
     }
 
     /**
      * {@inheritDoc}
      */
-    public void setLocations(Resource[] locations) {
-        this.m_locations = locations;
+    public final void setLocations(Resource[] locations) {
+        super.setLocations(locations);
+        m_locations = locations;
     }
 
     /**
      * {@inheritDoc}
      */
-    public void setFileEncoding(String encoding) {
-        this.m_fileEncoding = encoding;
+    public final void setFileEncoding(String encoding) {
+        super.setFileEncoding(encoding);
+        m_fileEncoding = encoding;
     }
 
     /**
      * {@inheritDoc}
      */
-    public void setPropertiesPersister(
-            PropertiesPersister propertiesPersister) {
-        this.m_propertiesPersister = propertiesPersister;
+    public final void setPropertiesPersister(
+        PropertiesPersister propertiesPersister) {
+        setPropertiesPersister(propertiesPersister);
+        m_propertiesPersister = propertiesPersister;
     }
 
     /**
      * {@inheritDoc}
      */
-    public void setIgnoreResourceNotFound(boolean ignoreResourceNotFound) {
-        this.m_ignoreResourceNotFound = ignoreResourceNotFound;
+    public final void setIgnoreResourceNotFound(
+        boolean ignoreResourceNotFound) {
+        setIgnoreResourceNotFound(ignoreResourceNotFound);
+        m_ignoreResourceNotFound = ignoreResourceNotFound;
+    }
+
+    /**
+     * @return Returns the fileEncoding.
+     */
+    public final String getFileEncoding() {
+        return m_fileEncoding;
+    }
+
+    /**
+     * @return Returns the ignoreResourceNotFound.
+     */
+    public final boolean isIgnoreResourceNotFound() {
+        return m_ignoreResourceNotFound;
+    }
+
+    /**
+     * @return Returns the locations.
+     */
+    public final Resource[] getLocations() {
+        return m_locations;
+    }
+
+    /**
+     * @return Returns the properties.
+     */
+    public final Properties getProperties() {
+        return m_properties;
+    }
+
+    /**
+     * @return Returns the propertiesPersister.
+     */
+    public final PropertiesPersister getPropertiesPersister() {
+        return m_propertiesPersister;
+    }
+
+    /**
+     * Marks if new items should be pre- or appended to the old values.
+     * 
+     * @return Returns the insertNewItemsAfter.
+     */
+    public final boolean isInsertNewItemsAfter() {
+        return m_insertNewItemsAfter;
+    }
+
+    /**
+     * Marks if new items should be pre- or appended to the old values.
+     * 
+     * @param insertNewItemsAfter The insertNewItemsAfter to set.
+     */
+    public final void setInsertNewItemsAfter(boolean insertNewItemsAfter) {
+        m_insertNewItemsAfter = insertNewItemsAfter;
+    }
+
+    /**
+     * Marks if new items should be pre- or appended to the old values.
+     * Is the opposite of method <code>isInsertNewItemsAfter</code>.
+     * 
+     * @return Returns the insertNewItemsBefore.
+     * @see #isInsertNewItemsAfter()
+     */
+    public final boolean isInsertNewItemsBefore() {
+        return !isInsertNewItemsAfter();
+    }
+
+    /**
+     * Marks if new items should be pre- or appended to the old values.
+     * Is the opposite of method <code>setInsertNewItemsAfter</code>.
+     * 
+     * @param insertNewItemsBefore The insertNewItemsBefore to set.
+     * @see #setInsertNewItemsAfter(boolean)
+     */
+    public final void setInsertNewItemsBefore(boolean insertNewItemsBefore) {
+        setInsertNewItemsAfter(!insertNewItemsBefore);
     }
 
     /**
      * {@inheritDoc}
      */
     public void postProcessBeanFactory(
-            ConfigurableListableBeanFactory beanFactory) {
+        ConfigurableListableBeanFactory beanFactory) {
         Properties mergedProps = new Properties();
 
-        if (this.m_properties != null) {
-            // use propertyNames enumeration to also catch default properties
-            for (Enumeration en = this.m_properties.propertyNames(); en
-                    .hasMoreElements();) {
-                String key = (String) en.nextElement();
-                mergedProps
-                        .setProperty(key, this.m_properties.getProperty(key));
-            }
-        }
+        addDefaultProperties(mergedProps);
 
-        if (this.m_locations != null) {
-            for (int i = 0; i < this.m_locations.length; i++) {
-                Resource location = this.m_locations[i];
-                if (s_logger.isInfoEnabled()) {
-                    s_logger.info("Loading properties from " + location + "");
-                }
-                try {
-                    InputStream is = location.getInputStream();
-                    try {
-                        if (this.m_fileEncoding != null) {
-                            this.m_propertiesPersister.load(mergedProps,
-                                    new InputStreamReader(is,
-                                            this.m_fileEncoding));
-                        } else {
-                            this.m_propertiesPersister.load(mergedProps, is);
-                        }
-                    } finally {
-                        is.close();
-                    }
-                } catch (IOException ex) {
-                    String msg = "Could not load properties from " + location;
-                    if (this.m_ignoreResourceNotFound) {
-                        if (s_logger.isWarnEnabled()) {
-                            s_logger.warn(msg + ": " + ex.getMessage());
-                        }
-                    } else {
-                        throw new BeanInitializationException(msg, ex);
-                    }
-                }
+        Resource[] locations = getLocations();
+        if (locations != null) {
+            for (int i = 0; i < locations.length; i++) {
+                Resource location = locations[i];
+                loadProperties(mergedProps, location);
                 processProperties(beanFactory, mergedProps);
                 mergedProps = new Properties();
             }
@@ -218,36 +253,129 @@ public class ListPropertyMergeConfigurer extends PropertyOverrideConfigurer {
     }
 
     /**
+     * Adds default properties to the properties container.
+     *  
+     * @param mergedProps Is the properties container to write into.
+     */
+    protected void addDefaultProperties(Properties mergedProps) {
+        Properties defaultProperties = getProperties();
+        if (defaultProperties != null) {
+            // use propertyNames enumeration to also catch default properties
+            Enumeration en = defaultProperties.propertyNames();
+            while (en.hasMoreElements()) {
+                String key = (String) en.nextElement();
+                mergedProps.setProperty(
+                    key, defaultProperties.getProperty(key));
+            }
+        }
+    }
+
+    /**
+     * Loads the properties form the given location.
+     * 
+     * @param mergedProps Is the properties container to write into.
+     * @param location Is the resource location to read properties from.
+     */
+    protected void loadProperties(Properties mergedProps, Resource location) {
+        if (logger.isInfoEnabled()) {
+            logger.info("Loading properties file from " + location);
+        }
+        InputStream is = null;
+        try {
+            is = location.getInputStream();
+            if (location.getFilename().endsWith(XML_FILE_EXTENSION)) {
+                getPropertiesPersister().loadFromXml(mergedProps, is);
+            } else {
+                String fileEncoding = getFileEncoding();
+                if (fileEncoding != null) {
+                    getPropertiesPersister().load(mergedProps, 
+                        new InputStreamReader(is, fileEncoding));
+                } else {
+                    getPropertiesPersister().load(mergedProps, is);
+                }
+            }
+        } catch (IOException ex) {
+            String msg = "Could not load properties from " + location;
+            if (isIgnoreResourceNotFound()) {
+                if (s_logger.isWarnEnabled()) {
+                    s_logger.warn(msg + ": " + ex.getMessage());
+                }
+            } else {
+                CoreNotificationHelper.notifyMisconfiguration(msg, ex);
+            }
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    if (s_logger.isWarnEnabled()) {
+                        s_logger.warn("Inputstream could not be "
+                            + "properly closed.", e);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
      * {@inheritDoc}
      */
     protected void applyPropertyValue(ConfigurableListableBeanFactory factory,
-            String beanName, String property, String value) {
-
-        BeanWrapperImpl bw = new BeanWrapperImpl();
-        BeanDefinition bd = factory.getBeanDefinition(beanName);
-        MutablePropertyValues mpv = bd.getPropertyValues();
-        PropertyValue pv = mpv.getPropertyValue(property);
-        List valueList;
-
-        if (pv != null) {
-            if (!(pv.getValue() instanceof List)) {
-                String[] oldValues = (String[]) bw.doTypeConversionIfNecessary(
-                        pv.getValue(), String[].class);
-                valueList = new ArrayList();
-                for (int i = 0; i < oldValues.length; i++) {
-                    valueList.add(oldValues[i].trim());
-                }
+        String beanName, String property, String value) {
+        /**
+         * Read the existing property value.
+         */
+        BeanWrapperImpl beanWrapper = new BeanWrapperImpl();
+        BeanDefinition beanDefinition = factory.getBeanDefinition(beanName);
+        MutablePropertyValues mutablePropertyValues 
+            = beanDefinition.getPropertyValues();
+        PropertyValue propertyValue 
+            = mutablePropertyValues.getPropertyValue(property);
+        
+        /**
+         * Fill the old values from property into list.
+         */
+        List oldValueList = new ArrayList();
+        if (propertyValue != null) {
+            Object valueObject = propertyValue.getValue();
+            if (valueObject instanceof Collection) {
+                oldValueList.addAll((Collection) valueObject);
             } else {
-                valueList = (List) pv.getValue();
+                String[] oldValues 
+                    = (String[]) beanWrapper.doTypeConversionIfNecessary(
+                        valueObject, String[].class);
+                for (int i = 0; i < oldValues.length; i++) {
+                    String oldValue = oldValues[i];
+                    oldValueList.add(
+                        oldValue != null ? oldValue.trim() : oldValue);
+                }
             }
-        } else {
-            valueList = new ArrayList();
         }
-        String[] newValues = (String[]) bw.doTypeConversionIfNecessary(value,
+        
+        /**
+         * Fill the new values into list.
+         */
+        List newValueList = new ArrayList();
+        String[] newValues 
+            = (String[]) beanWrapper.doTypeConversionIfNecessary(value,
                 String[].class);
         for (int i = 0; i < newValues.length; i++) {
-            valueList.add(newValues[i].trim());
+            String newValue = newValues[i];
+            newValueList.add(
+                newValue != null ? newValue.trim() : newValue);
         }
-        mpv.addPropertyValue(property, valueList);
+        
+        /**
+         * Mix the old and new values and set the new property value.
+         */
+        List mixedValueList = new ArrayList();
+        if (isInsertNewItemsAfter()) {
+            mixedValueList.addAll(oldValueList);
+            mixedValueList.addAll(newValueList);
+        } else {
+            mixedValueList.addAll(newValueList);
+            mixedValueList.addAll(oldValueList);
+        }
+        mutablePropertyValues.addPropertyValue(property, mixedValueList);
     }
 }
