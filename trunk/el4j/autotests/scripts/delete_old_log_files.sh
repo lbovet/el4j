85a6b1105ee64f985161a276c348791bfd74fda9
swisswheel <swisswheel@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-03-15 10:13:40 +0000

    SVN keywords added.

--- trunk/el4j/autotests/scripts/delete_old_log_files.sh
+++ trunk/el4j/autotests/scripts/delete_old_log_files.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# $URL$
+# $Revision$
+# $Date$
+# $Author$
+
 cd "${1}"
 shift 1
 
--- trunk/el4j/autotests/scripts/upload_framework_website.sh
+++ trunk/el4j/autotests/scripts/upload_framework_website.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# $URL$
+# $Revision$
+# $Date$
+# $Author$
+
 # Upload generated website to SourceForge
 rsync -rv --rsh=ssh /home/users2/tester/el4j/external/framework/dist/website/* swisswheel@shell.sf.net:/home/groups/e/el/el4j/htdocs/
 
