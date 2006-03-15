a8d77b782804b0fd39671793085bf2067068d588
swisswheel <swisswheel@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-03-15 11:14:17 +0000

    Env included in project scripts.

--- el4j/autotests/scripts/autotest_framework.sh
+++ el4j/autotests/scripts/autotest_framework.sh
@@ -9,7 +9,21 @@ DOTASK="${1}"
 shift 1
 
 # Setup the environment for project "framework"
-./setup_environment_framework.sh
+## Specific env variables for ELCA internal test server "leaffy"
+export BASH_ENV=$HOME/.bashrc
+export JAVA_HOME=/opt/bea/weblogic90/jdk1.5
+export PATH=$PATH:$HOME/bin:$JAVA_HOME/bin
+export ANT_OPTS="-Duser.language=en -Duser.region=US"
+
+## Home of EL4J autotest configuration and EL4J projects
+export BASEDIR=$HOME/el4j/external
+## Directory where to copy the generated website
+export REPORTDIR=/home/java/el4j/snapshot/website
+## URL for the REPORTDIR above
+export REPORTURL=http://leaffy/java/el4j/snapshot/website
+## Base directory to write log files
+export LOGBASEDIR="${BASEDIR}/autotests/logs"
+
 
 if [ -z "$LOGBASEDIR" ] || ! [ -d "$LOGBASEDIR" ] ; then
     echo "Env variable LOGBASEDIR must be set and point "
