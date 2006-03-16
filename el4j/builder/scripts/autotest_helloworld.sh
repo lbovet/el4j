fbff2c36b14f84dd7b530835b67b0ac2214cfd3d
swisswheel <swisswheel@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-03-16 08:00:27 +0000

    Directory "autotests" moved to "builder".

--- el4j/builder/scripts/autotest_helloworld.sh
+++ el4j/builder/scripts/autotest_helloworld.sh
@@ -9,14 +9,14 @@ DOTASK="${1}"
 shift 1
 
 # Setup the environment for project "helloworld"
-## Home of EL4J autotest configuration and EL4J projects
+## Home of EL4J builder configuration and EL4J projects
 export BASEDIR=$HOME/el4j/external
 ## Directory where to copy the generated website
 export REPORTDIR=/home/java/el4j/snapshot/website
 ## URL for the REPORTDIR above
 export REPORTURL=http://leaffy/java/el4j/snapshot/website
 ## Base directory to write log files
-export LOGBASEDIR="${BASEDIR}/autotests/logs"
+export LOGBASEDIR="${BASEDIR}/builder/logs"
 
 
 if [ -z "$LOGBASEDIR" ] || ! [ -d "$LOGBASEDIR" ] ; then
@@ -36,5 +36,5 @@ mkdir -p "${SPECLOGDIR}"
 ./delete_old_log_files.sh "${SPECLOGDIR}"
 
 echo "Executing task ${DOTASK} for external helloworld..."
-./autotest_project.sh "${DOTASK}" "helloworld" "helloworld" "helloworld-tests" "tomcat" &> ${SPECLOGPATH}
+./build_project.sh "${DOTASK}" "helloworld" "helloworld" "helloworld-tests" "tomcat" &> ${SPECLOGPATH}
 cat ${SPECLOGPATH}
