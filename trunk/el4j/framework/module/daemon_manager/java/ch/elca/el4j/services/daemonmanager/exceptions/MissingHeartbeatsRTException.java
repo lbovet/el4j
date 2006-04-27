841e9b6fc285771597a33734adb5c78b126fd479
swisswheel <swisswheel@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-04-27 15:14:17 +0000

    Tagging the EL4J release 1.0.0.

---
+++ trunk/el4j/framework/module/daemon_manager/java/ch/elca/el4j/services/daemonmanager/exceptions/MissingHeartbeatsRTException.java
@@ -0,0 +1,104 @@
+/*
+ * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
+ * the spring framework, http://el4j.sf.net
+ * Copyright (C) 2005 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * EL4J is published under the GNU General Public License (GPL) Version 2.0.
+ * http://www.gnu.org/licenses/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+
+package ch.elca.el4j.services.daemonmanager.exceptions;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.springframework.util.StringUtils;
+
+import ch.elca.el4j.core.exceptions.BaseRTException;
+import ch.elca.el4j.core.exceptions.MisconfigurationRTException;
+import ch.elca.el4j.services.daemonmanager.Daemon;
+import ch.elca.el4j.util.codingsupport.CollectionUtils;
+
+/**
+ * Exception to indicate that daemon heartbeats have missed.
+ *
+ * <script type="text/javascript">printFileStatus
+ *   ("$URL$",
+ *    "$Revision$",
+ *    "$Date$",
+ *    "$Author$"
+ * );</script>
+ *
+ * @author Martin Zeltner (MZE)
+ */
+public class MissingHeartbeatsRTException extends BaseRTException {
+    
+    /**
+     * Set of daemons which have missed to send heartbeats.
+     */
+    private final Set m_daemonsWhereHeartbeatMissed;
+    
+    /**
+     * Constructor which receives a list of deamos.
+     * 
+     * @param daemonsWhereHeartbeatMissed
+     *            Is a set of daemons which have missed to send heartbeats.
+     */
+    public MissingHeartbeatsRTException(Set daemonsWhereHeartbeatMissed) {
+        super("");
+        if (CollectionUtils.isEmpty(daemonsWhereHeartbeatMissed)) {
+            throw new MisconfigurationRTException(
+                "Daemon set must not be null or empty.");
+        }
+
+        if (!CollectionUtils.containsOnlyObjectsOfType(
+            daemonsWhereHeartbeatMissed, Daemon.class)) {
+            throw new MisconfigurationRTException(
+                "Daemon set must only contain classes of type '"
+                + Daemon.class.getName() + "'.");
+        }
+        
+        /**
+         * Copy daemons into new set.
+         */
+        m_daemonsWhereHeartbeatMissed 
+            = new HashSet(daemonsWhereHeartbeatMissed);
+        
+        /**
+         * Create human readable error message.
+         */
+        StringBuffer sb = new StringBuffer();
+        sb.append("There were heartbeats missed on following daemon(s): {");
+        Iterator it = daemonsWhereHeartbeatMissed.iterator();
+        boolean isNotFirst = false;
+        while (it.hasNext()) {
+            if (isNotFirst) {
+                sb.append(", ");
+            }
+            isNotFirst = true;
+            
+            Daemon daemon = (Daemon) it.next();
+            String identification = daemon.getIdentification();
+            sb.append(StringUtils.hasLength(identification) 
+                ? identification : "no identification");
+        }
+        sb.append("}.");
+        setFormatString(sb.toString());
+    }
+
+    /**
+     * @return Returns the daemonsWhereHeartbeatMissed.
+     */
+    public Set getDaemonsWhereHeartbeatMissed() {
+        return m_daemonsWhereHeartbeatMissed;
+    }
+}
---
+++ trunk/el4j/framework/tests/exception_handling/java/ch/elca/el4j/tests/services/exceptionhandler/A.java
@@ -0,0 +1,103 @@
+/*
+ * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
+ * the spring framework, http://el4j.sf.net
+ * Copyright (C) 2005 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * EL4J is published under the GNU General Public License (GPL) Version 2.0.
+ * http://www.gnu.org/licenses/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+
+package ch.elca.el4j.tests.services.exceptionhandler;
+
+/**
+ * Interface for testing purposes.
+ *
+ * <script type="text/javascript">printFileStatus
+ *   ("$URL$",
+ *    "$Revision$",
+ *    "$Date$",
+ *    "$Author$"
+ * );</script>
+ *
+ * @author Andreas Bur (ABU)
+ */
+public interface A extends Adder {
+
+    /**
+     * Sets the number of retries.
+     * 
+     * @param retries
+     *      Number of retries.
+     */
+    public abstract void setRetries(int retries);
+
+    /**
+     * Sets the adder to delegate calls to.
+     * 
+     * @param adder
+     *      The adder to set.
+     */
+    public abstract void setAdder(Adder adder);
+
+    /**
+     * Divides the two integers.
+     * 
+     * @param a
+     *      Dividend.
+     *      
+     * @param b
+     *      Divisor.
+     *      
+     * @return Returns a / b.
+     */
+    public abstract int div(int a, int b);
+
+    /**
+     * Throws an application level exception.
+     * 
+     * @throws ApplicationException
+     *      The sample exception.
+     */
+    public abstract void throwException() throws ApplicationException;
+
+    /**
+     * Throws a runtime exception.
+     */
+    public abstract void throwRTException();
+
+    /**
+     * Concatenates the two Strings.
+     * 
+     * @param a the first string.
+     * @param b the second string.
+     * @return Returns the concatenation of the two strings.
+     */
+    public abstract String concat(String a, String b);
+
+    /**
+     * {@inheritDoc}
+     */
+    public abstract int add(int a, int b);
+
+    /**
+     * Subtracts the given two numbers.
+     * 
+     * @param a
+     *      The first number.
+     *      
+     * @param b
+     *      The second number.
+     *      
+     * @return Returns a - b.
+     */
+    public abstract int sub(int a, int b);
+
+}
