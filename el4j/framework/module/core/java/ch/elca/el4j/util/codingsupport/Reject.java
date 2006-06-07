4d7e76d26dd65476eedb2f994dd44d3c795de52c
elca-ams <elca-ams@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-06-07 07:33:06 +0000

    Added overloaded methods that permit to customize the exception thrown in case the precondition is violated.

--- el4j/framework/module/core/java/ch/elca/el4j/util/codingsupport/Reject.java
+++ el4j/framework/module/core/java/ch/elca/el4j/util/codingsupport/Reject.java
@@ -18,6 +18,7 @@
 package ch.elca.el4j.util.codingsupport;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 
 import org.apache.commons.logging.Log;
@@ -46,10 +47,15 @@ import org.springframework.util.StringUtils;
  * </pre></code>
  * In this example a 
  * <code>ch.elca.el4j.util.codingsupport.PreconditionRTException</code> will be
- * thrown if the given account will be null. In that way we can pervent that an
- * ugli <code>NullPointerException</code> can be thrown. It is also possible to 
- * add a reason to the reject method, if it is not absolutly clear what is 
- * checked.
+ * thrown if the given account will be null. That way, we can prevent that an
+ * ugly <code>NullPointerException</code> can be thrown. 
+ * 
+ * <p> It is also possible to add a reason to the reject method, if it is not 
+ * absolutely clear what is checked. Alternatively, you can customize the
+ * exception to be thrown in case the condition is violated by providing the
+ * exception class and the arguments to its constructor. Existence and 
+ * uniqueness of a constructor capable of taking the provided arguments is not
+ * verified statically and must therefore be ensured by the user.  
  * 
  * <script type="text/javascript">printFileStatus
  *   ("$URL$",
@@ -58,7 +64,7 @@ import org.springframework.util.StringUtils;
  *    "$Author$"
  * );</script>
  *
- * @author Martin Zeltner (MZE)
+ * @author Martin Zeltner (MZE), Adrian Moos(AMS)
  */
 public final class Reject {
     /**
@@ -84,8 +90,9 @@ public final class Reject {
      *            Is the object to be analyzed.
      */
     public static void ifNull(Object object) {
-        ifNull(object, null);
+        ifNull(object, PreconditionRTException.class);
     }
+    
 
     /**
      * Method to ensure that an object is not null. Used at the beginning of a
@@ -97,9 +104,32 @@ public final class Reject {
      *            Is the message to explain the reason of the exception.
      */
     public static void ifNull(Object object, String reason) {
-        checkCondition(object != null, reason, PreconditionRTException.class);
+        ifNull(object, PreconditionRTException.class, reason);
     }
-
+    
+    
+    /**
+     * Method to ensure that an object is not null. Used at the beginning of a
+     * method before working with parameters.
+     * 
+     * @param object
+     *            Is the object to be analyzed.
+     * @param exceptionType
+     *            the kind of {@link PreconditionRTException} to be thrown in
+     *            case the precondition is violated.
+     * @param exceptionArguments 
+     *            constructor arguments for {@code exceptionType}.
+     */
+    public static 
+    void ifNull(Object object, 
+                Class<? extends PreconditionRTException> exceptionType,
+                Object... exceptionArguments) {
+        
+        checkCondition(object != null, exceptionType, exceptionArguments);
+    }
+    
+    
+    
     /**
      * Method to ensure that a condition is true. Used at the beginning of a
      * method before working with parameters.
@@ -108,7 +138,7 @@ public final class Reject {
      *            Is the condition to be analyzed.
      */
     public static void ifFalse(boolean condition) {
-        ifFalse(condition, null);
+        ifFalse(condition, PreconditionRTException.class);
     }
 
     /**
@@ -121,9 +151,30 @@ public final class Reject {
      *            Is the message to explain the reason of the exception.
      */
     public static void ifFalse(boolean condition, String reason) {
-        checkCondition(condition, reason, PreconditionRTException.class);
+        ifFalse(condition, PreconditionRTException.class, reason);
     }
 
+    /**
+     * Method to ensure that a condition is true. Used at the beginning of a
+     * method before working with parameters.
+     * 
+     * @param condition
+     *            Is the condition to be analyzed.
+     * @param exceptionType
+     *            the kind of {@link PreconditionRTException} to be thrown in
+     *            case the precondition is violated.
+     * @param exceptionArguments 
+     *            constructor arguments for {@code exceptionType}.
+     */
+    public static 
+    void ifFalse(boolean condition, 
+                 Class<? extends PreconditionRTException> exceptionType,
+                 Object... exceptionArguments) {
+        
+        checkCondition(condition, exceptionType, exceptionArguments);
+    }
+    
+    
     /**
      * Method to ensure that a string is not empty. Used at the beginning of a
      * method before working with parameters.
@@ -132,7 +183,7 @@ public final class Reject {
      *            Is the string to be analyzed.
      */
     public static void ifEmpty(String s) {
-        ifEmpty(s, null);
+        ifEmpty(s, PreconditionRTException.class);
     }
 
     /**
@@ -145,9 +196,33 @@ public final class Reject {
      *            Is the message to explain the reason of the exception.
      */
     public static void ifEmpty(String s, String reason) {
-        checkCondition(StringUtils.hasText(s), reason,
-                PreconditionRTException.class);
+        ifEmpty(s, PreconditionRTException.class, reason);
     }
+    
+    /**
+     * Method to ensure that a string is not empty. Used at the beginning of a
+     * method before working with parameters.
+     * 
+     * @param s
+     *            Is the string to be analyzed.
+     * @param exceptionType
+     *            the kind of {@link PreconditionRTException} to be thrown in
+     *            case the precondition is violated.
+     * @param exceptionArguments 
+     *            constructor arguments for {@code exceptionType}.
+     */
+    public static 
+    void ifEmpty(String s, 
+                 Class<? extends PreconditionRTException> exceptionType,
+                 Object... exceptionArguments) {
+        
+        checkCondition(
+            StringUtils.hasText(s), 
+            exceptionType, 
+            exceptionArguments
+        );
+    }
+    
     
     /**
      * Method to ensure that a collection is not empty.
@@ -155,8 +230,8 @@ public final class Reject {
      * @param c
      *            The collection.
      */
-    public static void ifEmpty(Collection c) {
-        ifEmpty(c, null);
+    public static void ifEmpty(Collection<?> c) {
+        ifEmpty(c, PreconditionRTException.class);
     }
     
     /**
@@ -167,10 +242,33 @@ public final class Reject {
      * @param reason
      *            Message that explains the reason of the thrown exception.
      */
-    public static void ifEmpty(Collection c, String reason) {
-        checkCondition(!CollectionUtils.isEmpty(c), reason,
-                PreconditionRTException.class);
+    public static void ifEmpty(Collection<?> c, String reason) {
+        ifEmpty(c, PreconditionRTException.class, reason);
     }
+    
+    /**
+     * Method to ensure that a collection is not empty.
+     * 
+     * @param c
+     *            The collection.    
+     * @param exceptionType
+     *            the kind of {@link PreconditionRTException} to be thrown in
+     *            case the precondition is violated.
+     * @param exceptionArguments 
+     *            constructor arguments for {@code exceptionType}.
+     */
+    public static 
+    void ifEmpty(Collection<?> c, 
+                 Class<? extends PreconditionRTException> exceptionType,
+                 Object... exceptionArguments) {
+       
+        checkCondition(
+            !CollectionUtils.isEmpty(c),
+            exceptionType,
+            exceptionArguments
+        );
+    }
+
 
     ////////////////////
     // Common methods //
@@ -182,46 +280,57 @@ public final class Reject {
      * 
      * @param condition
      *            Is the condition to check.
-     * @param reason
-     *            Is the message to explain the reason of the exception.
      * @param conditionException
      *            Is the exception which will be thrown.
+     * @param constructorArgs
+     *            Are the arguments to the exception's constructor.
      */
-    private static void checkCondition(boolean condition, String reason,
-            Class conditionException) {
+    private static void 
+    checkCondition(boolean condition,
+                   Class<? extends PreconditionRTException> conditionException,
+                   Object... constructorArgs) {
+        
         if (!condition) {
-            AbstractConditionRTException e = getConditionException(reason,
-                    conditionException);
+            AbstractConditionRTException e = instantiateConditionException(
+                conditionException,
+                constructorArgs
+            );
             s_logger.error(e.getMessage());
             throw e;
         }
-
     }
-
-    /**
-     * This method creates an instance of the desired exception.
-     * 
-     * @param reason
-     *            Is the message to explain the reason of the exception.
-     * @param conditionException
-     *            Is the exception which will be returned.
-     * @return Returns the created instance of a AbstractConditionRTException.
-     */
-    private static AbstractConditionRTException getConditionException(
-        String reason, Class conditionException) {
-        try {
-            if (reason != null && reason.length() > 0) {
-                Constructor c = conditionException
-                        .getConstructor(new Class[] {String.class});
-                return (AbstractConditionRTException) c
-                        .newInstance(new Object[] {reason});
-            } else {
-                return (AbstractConditionRTException) conditionException
-                    .newInstance();
+    
+    /** instantiates an AbstractConditionRTException of the supplied type
+     * using the supplied constructor arguments. 
+     **/
+    private static <T extends AbstractConditionRTException> 
+    T instantiateConditionException(Class<T> exceptionClass, 
+                                    Object... constructorArgs) {
+        
+        for (Constructor<T> c : exceptionClass.getConstructors()) {
+            try {
+                return c.newInstance(constructorArgs);
+            } catch (IllegalArgumentException e) {
+                continue;
+            } catch (InstantiationException e) {
+                // exceptionClass is abstract
+                assert false;
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                continue;
+            } catch (InvocationTargetException e) {
+                assert false;
+                throw new RuntimeException(
+                    "error instantiating " + exceptionClass.getName()
+                    + ": constructor threw ", e
+                );
             }
-        } catch (Exception e) {
-            assert false;
-            throw new RuntimeException(e);
         }
+        assert false;
+        throw new IllegalArgumentException(
+            "error instantiating " + exceptionClass.getName() + ": " 
+            + "there appears to be no accessible constructor accepting the "
+            + "provided arguments."
+        );
     }
 }
