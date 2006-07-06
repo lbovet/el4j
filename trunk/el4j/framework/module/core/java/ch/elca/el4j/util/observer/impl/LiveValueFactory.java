873f54165123a9e82131e48ee64b705612e7a3c3
elca-ams <elca-ams@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-07-06 15:41:44 +0000

    Yet a better interface for LiveValueFactory.theElementIn.

--- trunk/el4j/framework/module/core/java/ch/elca/el4j/util/observer/impl/LiveValueFactory.java
+++ trunk/el4j/framework/module/core/java/ch/elca/el4j/util/observer/impl/LiveValueFactory.java
@@ -35,59 +35,31 @@ import ch.elca.el4j.util.observer.ObservableValue;
 public final class LiveValueFactory {
     /** This class can not be instantiated. */
     private LiveValueFactory() { }
-    
-    /**
-     * Casts the argument. The cast is safe due to ObservableValue's type-
-     * parameter covariance. 
-     */
-    // Patch a weakness in the typesystem using the loophole opened by 
-    // type erasure.
-    @SuppressWarnings("unchecked")
-    public static <T, S extends T> 
-    ObservableValue<T> cast(ObservableValue<S> o) {
-        // there are two steps to the cast because the compiler is stubborn.
-        return (ObservableValue<T>) (ObservableValue<?>) o;
-    }
-
-    /** 
-     * Returns a LiveValue backed by a Collection that yields the collection's
-     * unique element or <code>null</code> if there is no such element.
-     * 
-     * <p> Use the version with the W-suffix if the argument is a 
-     * wildcard type.
-
-     * @param oc the backing collection
-     * @return see above. 
-     */
-    public static <T> 
-    LiveValue<T> theElementIn(ObservableValue<Collection<T>> oc) {
-        ObservableValue<Collection<? extends T>> coc = cast(oc);
-        return theElementInW(coc);
-    }
-
-    /** 
-     * Returns a LiveValue backed by a Collection that yields the collection's
-     * unique element or <code>null</code> if there is no such element.
-     * 
-     * <p> Use the version without the W-suffix if the argument is not a 
-     * wildcard type.
-     * 
-     * @param oc the backing collection
-     * @return see above. 
-     */
-    public static <T> LiveValue<T> theElementInW(
-        ObservableValue<Collection<? extends T>> oc) {
         
-        return new LiveValue<T>(new UniqueElementIn<T>(oc));
+    /** 
+     * Returns a LiveValue backed by a Collection that yields the collection's
+     * unique element or <code>null</code> if there is no such element.
+     * 
+     * @param oc the backing collection
+     * @return see above. 
+     */
+    public static
+        <T, OC extends ObservableValue<? extends Collection<? extends T>>> 
+    LiveValue<T> theElementIn(OC oc) {
+        
+        return new LiveValue<T>(new UniqueElementIn<T, OC>(oc));
     }
     
     /** see {@link LiveValueFactory#theElementIn(AbstractObservableValue)}. */
-    private static class UniqueElementIn<T> implements Computable<T> {
+    private static class UniqueElementIn
+        <T, OC extends ObservableValue<? extends Collection<? extends T>>>
+        implements Computable<T> {
+        
         /** The backing collection. */
-        ObservableValue<Collection<? extends T>> m_oc;
+        OC m_oc;
         
         /** Constructor. */
-        UniqueElementIn(ObservableValue<Collection<? extends T>> oc) {
+        UniqueElementIn(OC oc) {
             this.m_oc = oc;
         }
         
