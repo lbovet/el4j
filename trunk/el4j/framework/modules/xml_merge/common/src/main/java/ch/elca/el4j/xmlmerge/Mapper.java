926a64feac097ffe33dba3d19410e11ce6a03ae7
mathey <mathey@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-08-28 13:04:56 +0000

    Added xmlmerge module.

---
+++ trunk/el4j/framework/modules/xml_merge/common/src/main/java/ch/elca/el4j/xmlmerge/Mapper.java
@@ -0,0 +1,45 @@
+/*
+ * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
+ * the spring framework, http://el4j.sf.net
+ * Copyright (C) 2006 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * EL4J is published under the GNU General Public License (GPL) Version 2.0.
+ * http://www.gnu.org/licenses/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+package ch.elca.el4j.xmlmerge;
+
+import org.jdom.Element;
+
+/**
+ * Transforms an element to another element by changing some nested content or
+ * attributes.
+ *
+ * <script type="text/javascript">printFileStatus
+ *   ("$$URL$$",
+ *    "$$Revision$$",
+ *    "$$Date$$",
+ *    "$$Author$$"
+ * );</script>
+ * 
+ * @author Laurent Bovet (LBO)
+ * @author Alex Mathey (AMA)
+ */
+public interface Mapper extends Operation {
+
+    /**
+     * Transforms the given element to another element by changing some nested
+     * content or attributes.
+     * 
+     * @param patchElement the element to transform
+     * @return the transformed element
+     */
+    public Element map(Element patchElement);
+}
