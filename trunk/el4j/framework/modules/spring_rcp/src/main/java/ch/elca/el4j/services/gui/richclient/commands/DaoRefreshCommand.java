5d1febd2f5efd488cd6c493862d7f98933386f09
mathey <mathey@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-09-04 11:54:38 +0000

    Performed the following changes to the Spring RCP module: - Replaced the 'repository' name everywhere by the 'dao' name. - Made the repository classes checkstyle-conform.

---
+++ trunk/el4j/framework/modules/spring_rcp/src/main/java/ch/elca/el4j/services/gui/richclient/commands/DaoRefreshCommand.java
@@ -0,0 +1,56 @@
+/*
+ * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
+ * the spring framework, http://el4j.sf.net
+ * Copyright (C) 2006 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * EL4J is published under the GNU General Public License (GPL) Version 2.0.
+ * http://www.gnu.org/licenses/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+package ch.elca.el4j.services.gui.richclient.commands;
+
+import org.springframework.richclient.command.support.ApplicationWindowAwareCommand;
+
+import ch.elca.el4j.services.persistence.generic.DaoAgency;
+
+import static ch.elca.el4j.services.persistence.generic.dao.DaoChangeNotifier.FUZZY_CHANGE;
+
+/**
+ * A command to request refreshing a {@link DaoAgency}'s data.
+ *
+ * <script type="text/javascript">printFileStatus
+ *   ("$URL$",
+ *    "$Revision$",
+ *    "$Date$",
+ *    "$Author$"
+ * );</script>
+ *
+ * @author Adrian Moos (AMS)
+ */
+public class DaoRefreshCommand extends ApplicationWindowAwareCommand {
+    /** The dao agency  used by the components to be refreshed. */
+    protected DaoAgency m_agency;
+    
+    /**
+     * Default constructor.
+     * 
+     * @param agency The agency for which the data should be refreshed
+     */
+    public DaoRefreshCommand(DaoAgency agency) {
+        super("refreshCommand");
+        m_agency = agency;
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    protected void doExecuteCommand() {
+        m_agency.changed(FUZZY_CHANGE);
+    }
+}
