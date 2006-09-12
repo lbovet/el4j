9ae95a966e637cd1567ebb7a697e4b388f6bc949
swisswheel <swisswheel@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-09-12 06:38:50 +0000

    Exclude counterpieces added and plugin refactored.

--- trunk/el4j/plugins/maven-file-collector-plugin/src/main/java/ch/elca/el4j/plugins/filecollector/AggregateFilesMojo.java
+++ trunk/el4j/plugins/maven-file-collector-plugin/src/main/java/ch/elca/el4j/plugins/filecollector/AggregateFilesMojo.java
@@ -29,8 +29,10 @@ import org.apache.maven.project.MavenProject;
 import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.util.Assert;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.util.PathMatcher;
 import org.springframework.util.StringUtils;
 
 /**
@@ -58,22 +60,37 @@ public class AggregateFilesMojo extends AbstractMojo {
     protected File rootSourceDirectory;
     
     /**
-     * Comma separated list of patterns to lookup directories where to find 
-     * source files.
+     * Comma separated list of include patterns to lookup directories where to
+     * find source files.
      * 
-     * @parameter expression="${aggregate.sourceDirectoryPatterns}"
+     * @parameter expression="${aggregate.sourceDirectoryIncludePatterns}"
      * @required
      */
-    protected String sourceDirectoryPatterns;
+    protected String sourceDirectoryIncludePatterns;
     
     /**
-     * Comma separated list of patterns to lookup source files.
+     * Comma separated list of exclude patterns to lookup directories where to
+     * find source files.
      * 
-     * @parameter expression="${aggregate.sourceFilePatterns}"
+     * @parameter expression="${aggregate.sourceDirectoryExcludePatterns}"
+     */
+    protected String sourceDirectoryExcludePatterns;
+
+    /**
+     * Comma separated list of include patterns to lookup source files.
+     * 
+     * @parameter expression="${aggregate.sourceFileIncludePatterns}"
      * @required
      */
-    protected String sourceFilePatterns;
-    
+    protected String sourceFileIncludePatterns;
+
+    /**
+     * Comma separated list of exclude patterns to lookup source files.
+     * 
+     * @parameter expression="${aggregate.sourceFileExcludePatterns}"
+     */
+    protected String sourceFileExcludePatterns;
+
     /**
      * Directory where to aggregate files.
      * 
@@ -111,6 +128,31 @@ public class AggregateFilesMojo extends AbstractMojo {
     // Checkstyle: MemberName on
     
     /**
+     * Is the canonical path of the root source dir.
+     */
+    protected String m_rootSourceDirectoryString;
+    
+    /**
+     * Are the dir include patterns.
+     */
+    protected String[] m_dirIncludePatterns;
+
+    /**
+     * Are the dir exclude patterns.
+     */
+    protected String[] m_dirExcludePatterns;
+    
+    /**
+     * Are the file include patterns.
+     */
+    protected String[] m_fileIncludePatterns;
+
+    /**
+     * Are the file exclude patterns.
+     */
+    protected String[] m_fileExcludePatterns;
+
+    /**
      * {@inheritDoc}
      */
     public void execute() throws MojoExecutionException, MojoFailureException {
@@ -122,47 +164,73 @@ public class AggregateFilesMojo extends AbstractMojo {
             return;
         }
         
-        // rootSourceDirectory checks
-        Assert.isTrue(rootSourceDirectory != null 
-            && rootSourceDirectory.isDirectory() 
-            && rootSourceDirectory.canRead(), 
-            "Given root source directory must be a readable directory!");
-        String rootSourceDirectoryString = null;
-        try {
-            rootSourceDirectoryString = rootSourceDirectory.getCanonicalPath();
-        } catch (IOException e) {
-            throw new MojoExecutionException(
-                "Could not get canonical path for rootSourceDirectory.");
-        }
-        
-        // sourceDirectoryPatterns checks
-        if (!StringUtils.hasText(sourceDirectoryPatterns)) {
-            sourceDirectoryPatterns = null;
-        }
-        
-        // sourceFilePatterns checks
-        Assert.hasText(sourceFilePatterns, 
-            "Minimum one source file pattern must be given!");
-        
-        // targetDirectory checks
-        Assert.notNull(targetDirectory, "No target directory given!");
-        targetDirectory.mkdirs();
+        init();
 
         PathMatchingResourcePatternResolver resolver 
             = new PathMatchingResourcePatternResolver(
                 new FileSystemResourceLoader());
+        PathMatcher pathMatcher = new AntPathMatcher();
+        resolver.setPathMatcher(pathMatcher);
         
-        // Lookup source directories
+        List<File> sourceDirectories = resolveSourceDirectories(resolver);
+        
+        if (sourceDirectories.size() <= 0) {
+            log.warn("No source directories found with given source "
+                + "directory patterns '" + sourceDirectoryIncludePatterns 
+                + "'. File aggregation will be skipped.");
+            return;
+        }
+        
+        if (log.isInfoEnabled()) {
+            log.info("Following " + sourceDirectories.size()
+                + " source dir(s) could be found:");
+            for (File sourceDirectory : sourceDirectories) {
+                log.info(sourceDirectory.getAbsolutePath());
+            }
+        }
+        
+        resolveSourceFiles(resolver, sourceDirectories);
+    }
+
+    /**
+     * Resolves the source directories.
+     * 
+     * @param resolver Is the resolver used for dir lookup.
+     * @return Returns the list of source dirs.
+     * @throws MojoExecutionException On any execution problem.
+     */
+    protected List<File> resolveSourceDirectories(
+        PathMatchingResourcePatternResolver resolver)
+        throws MojoExecutionException {
+        
+        Log log = getLog();
+    
+        PathMatcher pathMatcher = resolver.getPathMatcher();
         List<File> sourceDirectories = new ArrayList<File>();
-        if (sourceDirectoryPatterns != null) {
-            String[] dirPatterns = sourceDirectoryPatterns.split(",");
-            for (String dirPattern : dirPatterns) {
+        if (sourceDirectoryIncludePatterns != null) {
+            for (String dirPattern : m_dirIncludePatterns) {
                 try {
                     Resource[] resources = resolver.getResources(
-                        rootSourceDirectoryString + "/" + dirPattern);
+                        m_rootSourceDirectoryString + "/" + dirPattern);
                     for (Resource resource : resources) {
                         File dir = resource.getFile();
                         if (dir.isDirectory()) {
+                            // Check if the given source dir should be excluded.
+                            boolean matchesExclude = false;
+                            if (m_dirExcludePatterns != null) {
+                                String dirString = StringUtils.replace(
+                                    dir.getCanonicalPath(), 
+                                    File.separator, "/");
+                                for (int i = 0; !matchesExclude 
+                                    && i < m_dirExcludePatterns.length; i++) {
+                                    matchesExclude = pathMatcher.match(
+                                        m_dirExcludePatterns[i], dirString);
+                                }
+                            }
+                            if (matchesExclude) {
+                                // Skip current source dir.
+                                continue;
+                            }
                             sourceDirectories.add(dir);
                         }
                     }
@@ -176,68 +244,70 @@ public class AggregateFilesMojo extends AbstractMojo {
                 + "source directory as the only one source directory.");
             sourceDirectories.add(rootSourceDirectory);
         }
+        return sourceDirectories;
+    }
+
+    /**
+     * Resolves the source directories.
+     * 
+     * @param resolver Is the resolver used for file lookup.
+     * @param sourceDirectories Are the src dirs where to lookup files.
+     * @throws MojoExecutionException On any execution problem.
+     */
+    protected void resolveSourceFiles(
+        PathMatchingResourcePatternResolver resolver, 
+        List<File> sourceDirectories) throws MojoExecutionException {
         
-        if (sourceDirectories.size() <= 0) {
-            log.warn("No source directories found with given source "
-                + "directory patterns '" + sourceDirectoryPatterns 
-                + "'. File aggregation will be skipped.");
-            return;
-        }
+        Log log = getLog();
         
-        if (log.isInfoEnabled()) {
-            log.info("Following " + sourceDirectories.size()
-                + " source dir(s) could be found:");
-            for (File sourceDirectory : sourceDirectories) {
-                log.info(sourceDirectory.getAbsolutePath());
-            }
-        }
+        PathMatcher pathMatcher = resolver.getPathMatcher();
         
-        String[] filePatterns = sourceFilePatterns.split(",");
-        log.info("Following source file patterns are present: " 
-            + sourceFilePatterns);
         for (File sourceDirectory : sourceDirectories) {
             String sourceDirectoryString;
             try {
-                sourceDirectoryString = sourceDirectory.getCanonicalPath();
+                sourceDirectoryString = StringUtils.replace(
+                    sourceDirectory.getCanonicalPath(), File.separator, "/");
                 int sourceDirectoryLength = sourceDirectoryString.length();
-                for (String filePattern : filePatterns) {
+                for (String filePattern : m_fileIncludePatterns) {
                     int fileCounter = 0;
                     Resource[] resources = resolver.getResources(
                         sourceDirectoryString + "/" + filePattern);
                     for (Resource resource : resources) {
                         File sourceFile = resource.getFile();
-                        String sourceFileString = sourceFile.getCanonicalPath();
+                        String sourceFileString = StringUtils.replace(
+                            sourceFile.getCanonicalPath(), File.separator, "/");
+                        
+                        // Check if the current resource is inside the source
+                        // dir.
                         if (!sourceFileString.startsWith(
                             sourceDirectoryString)) {
                             throw new MojoExecutionException("Looked up file '" 
                                 + sourceFileString 
                                 + "' is not from source dir '" 
-                                + rootSourceDirectoryString + "'!");
+                                + m_rootSourceDirectoryString + "'!");
                         }
+                        
+                        // Check if file should be excluded.
+                        boolean matchesExclude = false;
+                        if (m_fileExcludePatterns != null) {
+                            for (int i = 0; !matchesExclude 
+                                && i < m_fileExcludePatterns.length; i++) {
+                                matchesExclude = pathMatcher.match(
+                                    m_fileExcludePatterns[i], sourceFileString);
+                            }
+                        }
+                        if (matchesExclude) {
+                            // Skip current resource.
+                            continue;
+                        }
+
                         String relativeFilePath 
                             = sourceFileString.substring(sourceDirectoryLength);
-                        while (relativeFilePath.startsWith("/") 
-                            || relativeFilePath.startsWith("\\")) {
+                        while (relativeFilePath.startsWith("/")) {
                             relativeFilePath = relativeFilePath.substring(1);
                         }
-                        File targetFile 
-                            = new File(targetDirectory, relativeFilePath);
-                        if (!allowOverwrite && targetFile.exists()) {
-                            throw new MojoExecutionException(
-                                "Overwrite not allowed! Target file '" 
-                                + targetFile.getAbsolutePath() 
-                                + "' does already exists.");
-                        }
-//                        log.info("Copy file '" + sourceFile.getAbsolutePath() 
-//                            + "' to '" + targetFile.getAbsolutePath() + "'.");
-                        
-                        // Create target directories if necessary.
-                        File targetFileDir = targetFile.getParentFile();
-                        if (targetFileDir != null) {
-                            targetFileDir.mkdirs();
-                        }
-                        
-                        FileCopyUtils.copy(sourceFile, targetFile);
+
+                        copyFile(sourceFile, relativeFilePath);
                         fileCounter++;
                     }
                     log.info(fileCounter + " file(s) copied from directory '" 
@@ -249,8 +319,131 @@ public class AggregateFilesMojo extends AbstractMojo {
                     "Exception occured while lookup/copying source files.", e);
             }
         }
-        
+    }
 
+    /**
+     * Copies the given source file with the given relative file path.
+     * 
+     * @param sourceFile Is the source file to copy.
+     * @param relativeFilePath Is the relative path to copy.
+     * @return Returns the copied file.
+     * @throws MojoExecutionException On any execution exception.
+     * @throws IOException On any copy problems. 
+     */
+    protected File copyFile(File sourceFile, String relativeFilePath)
+        throws MojoExecutionException, IOException {
+        File targetFile 
+            = new File(targetDirectory, relativeFilePath);
+        if (!allowOverwrite && targetFile.exists()) {
+            throw new MojoExecutionException(
+                "Overwrite not allowed! Target file '" 
+                + targetFile.getAbsolutePath() 
+                + "' does already exists.");
+        }
+
+        // Create target directories if necessary.
+        File targetFileDir = targetFile.getParentFile();
+        if (targetFileDir != null) {
+            targetFileDir.mkdirs();
+        }
+
+        // Copy file.
+        FileCopyUtils.copy(sourceFile, targetFile);
         
+        return targetFile;
+    }
+    
+    /**
+     * Initializes the mojo.
+     * 
+     * @throws MojoExecutionException On any problem while execution.
+     */
+    protected void init() throws MojoExecutionException {
+        initPaths();
+        initPatterns();
+    }
+
+    /**
+     * Initializes the the paths.
+     * 
+     * @throws MojoExecutionException On any problem while execution.
+     */
+    protected void initPaths() throws MojoExecutionException {
+        // rootSourceDirectory checks
+        Assert.isTrue(rootSourceDirectory != null 
+            && rootSourceDirectory.isDirectory() 
+            && rootSourceDirectory.canRead(), 
+            "Given root source directory must be a readable directory!");
+        try {
+            m_rootSourceDirectoryString 
+                = rootSourceDirectory.getCanonicalPath();
+        } catch (IOException e) {
+            throw new MojoExecutionException(
+                "Could not get canonical path for rootSourceDirectory.");
+        }
+        
+        // targetDirectory checks
+        Assert.notNull(targetDirectory, "No target directory given!");
+        targetDirectory.mkdirs();
+    }
+
+    /**
+     * Initializes the patterns.
+     * 
+     * @throws MojoExecutionException On any problem while execution.
+     */
+    protected void initPatterns() throws MojoExecutionException {
+        Log log = getLog();
+        
+        // pattern checks
+        if (!StringUtils.hasText(sourceDirectoryIncludePatterns)) {
+            sourceDirectoryIncludePatterns = null;
+        }
+        if (!StringUtils.hasText(sourceDirectoryExcludePatterns)) {
+            sourceDirectoryExcludePatterns = null;
+        }
+        if (!StringUtils.hasText(sourceFileExcludePatterns)) {
+            sourceFileExcludePatterns = null;
+        }
+        
+        // sourceFilePatterns checks
+        Assert.hasText(sourceFileIncludePatterns, 
+            "Minimum one source file pattern must be given!");
+        
+        // Initializing source dir include patterns array
+        if (sourceDirectoryIncludePatterns != null) {
+            m_dirIncludePatterns = sourceDirectoryIncludePatterns.split(",");
+            log.info("Following source dir include patterns are present: " 
+                + sourceDirectoryIncludePatterns);
+        } else {
+            log.info("No source dir include patterns are present.");
+        }
+        
+        // Initializing source dir exclude patterns array
+        if (sourceDirectoryExcludePatterns != null) {
+            m_dirExcludePatterns = sourceDirectoryExcludePatterns.split(",");
+            log.info("Following source dir exclude patterns are present: " 
+                + sourceDirectoryExcludePatterns);
+        } else {
+            log.info("No source dir exclude patterns are present.");
+        }
+
+        // Initializing source file include patterns array
+        if (sourceFileIncludePatterns != null) {
+            m_fileIncludePatterns = sourceFileIncludePatterns.split(",");
+            log.info("Following source file include patterns are present: " 
+                + sourceFileIncludePatterns);
+        } else {
+            log.info("No source file include patterns are present.");
+        }
+        
+        // Initializing source file exclude patterns array
+        if (sourceFileExcludePatterns != null) {
+            m_fileExcludePatterns = sourceFileExcludePatterns.split(",");
+            log.info("Following source file exclude patterns are present: " 
+                + sourceFileExcludePatterns);
+        } else {
+            log.info("No source file exclude patterns are present.");
+        }
     }
 }
