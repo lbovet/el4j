5ed5b0c691babb4242f9d5c25d7d812ff6c32132
swisswheel <swisswheel@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-10-12 09:34:13 +0000

    Tests for convenience metadata support added.

---
+++ trunk/el4j/framework/modules/core/src/test/java/ch/elca/el4j/tests/util/metadata/attributes/ExampleAttributeTwo.java
@@ -0,0 +1,68 @@
+/*
+ * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
+ * the spring framework, http://el4j.sf.net
+ * Copyright (C) 2005 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * EL4J is published under the GNU General Public License (GPL) Version 2.0.
+ * http://www.gnu.org/licenses/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+
+package ch.elca.el4j.tests.util.metadata.attributes;
+
+/**
+ * An ExampleAttributeOne which only contains a private member in order to store
+ * a multiplication factor after multiplying the given factor by 10.
+ * 
+ * <script type="text/javascript">printFileStatus
+ *   ("$URL$",
+ *    "$Revision$",
+ *    "$Date$",
+ *    "$Author$"
+ * );</script>
+ * 
+ * @author Raphael Boog (RBO)
+ */
+public class ExampleAttributeTwo implements ExampleAttributeInterface {
+
+    /**
+     * The constant number to be multiplied with second factor.
+     */
+    public static final int CONSTANT_FACTOR = 10;
+
+    /**
+     * Factor member of this class.
+     */
+    private int m_factor;
+
+    /**
+     * Constructor which takes the factor and saves it.
+     * 
+     * @param factor
+     *            The factor to set.
+     */
+    public ExampleAttributeTwo(int factor) {
+        setFactor(factor);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getFactor() {
+        return m_factor;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setFactor(int factor) {
+        m_factor = factor * CONSTANT_FACTOR;
+    }
+}
