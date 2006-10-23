71ced2b196d466a76d613d94f9530ac4df6f3f87
swisswheel <swisswheel@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-10-23 14:12:13 +0000

    Ordering of config locations improved. Ordering of resource fetching added. Tests added for the resource ordering and bean definition loading order.

--- el4j/framework/modules/core/src/main/java/ch/elca/el4j/core/io/support/ListResourcePatternResolverDecorator.java
+++ el4j/framework/modules/core/src/main/java/ch/elca/el4j/core/io/support/ListResourcePatternResolverDecorator.java
@@ -18,9 +18,13 @@
 package ch.elca.el4j.core.io.support;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.core.io.Resource;
@@ -29,6 +33,8 @@ import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
 import org.springframework.util.StringUtils;
+
+import ch.elca.el4j.services.monitoring.notification.CoreNotificationHelper;
 
 /**
  * This class resolves resources using a list of resource names to preserve
@@ -49,21 +55,21 @@ import org.springframework.util.StringUtils;
 public class ListResourcePatternResolverDecorator
     implements ResourcePatternResolver {
 
-    /** Prefix for single classpath resources. */
-    public static final String CLASSPATH_RESOURCE = "classpath:";
-    
     /** The static logger. */
     private static Log s_logger = LogFactory.getLog(
             ListResourcePatternResolverDecorator.class);
+    
+    /** The (ordered) list of configuration locations. */
+    private final String[] m_configLocations;
+    
+    /** The (ordered) list of config location resources. */
+    private final Resource[] m_configLocationResources;
     
     /** 
      * The resource pattern resolver, where unsatisfied requests are delegated
      * to.
      */
     private ResourcePatternResolver m_patternResolver;
-    
-    /** The (ordered) list of configuration locations. */
-    private String[] m_locations;
     
     /** A path matcher providing a specific wildcard notation. */
     private PathMatcher m_pathMatcher;
@@ -75,6 +81,14 @@ public class ListResourcePatternResolverDecorator
     private boolean m_mergeWithOuterResources = false;
     
     /**
+     * Indicates if the most specific resource should be the last resource
+     * in the fetched resource array. If its value is set to <code>true</code>
+     * and only one resource is requested the least specific resource will be
+     * returned. Default is set to <code>false</code>.
+     */
+    private boolean m_mostSpecificResourceLast = false;
+
+    /**
      * Creates a new instance using a {@link
      * PathMatchingResourcePatternResolver} to delegate unresolved requests to
      * and an {@link AntPathMatcher} to interprete wildcard notations.
@@ -85,11 +99,25 @@ public class ListResourcePatternResolverDecorator
     public ListResourcePatternResolverDecorator(
             ConfigLocationProvider locationProvider) {
         
-        this(locationProvider,
-                new PathMatchingResourcePatternResolver(),
+        this(locationProvider, new PathMatchingResourcePatternResolver(),
                 new AntPathMatcher());
     }
     
+    /**
+     * Creates a new instance using a {@link AntPathMatcher} to interprete
+     * wildcard notations.
+     * 
+     * @param locationProvider
+     *            The configuration location provider to get the locations from.
+     * @param patternResovler
+     *            The pattern resolver to delegate unresolved requests to.
+     */
+    public ListResourcePatternResolverDecorator(
+        ConfigLocationProvider locationProvider,
+        ResourcePatternResolver patternResovler) {
+        this(locationProvider, patternResovler, new AntPathMatcher());
+    }
+
     /**
      * Creates a new instance, that is fully configured through constructor
      * arguments.
@@ -108,11 +136,35 @@ public class ListResourcePatternResolverDecorator
             ResourcePatternResolver patternResovler,
             PathMatcher pathMatcher) {
         
-        m_locations = locationProvider.getConfigLocations();
+        m_configLocations = locationProvider.getConfigLocations();
+        m_configLocationResources
+            = locationProvider.getConfigLocationResources();
+        
+        if (m_configLocations.length != m_configLocationResources.length) {
+            CoreNotificationHelper.notifyMisconfiguration(
+                "Number of config locations (size = {0}) is not equals the "
+                + "number of config location resources (size = {1})!", 
+                m_configLocations.length, m_configLocationResources.length);
+        }
+        
         m_patternResolver = patternResovler;
         m_pathMatcher = pathMatcher;
     }
     
+    /**
+     * @return Returns the patternResolver.
+     */
+    public ResourcePatternResolver getPatternResolver() {
+        return m_patternResolver;
+    }
+
+    /**
+     * @param patternResolver Is the patternResolver to set.
+     */
+    public void setPatternResolver(ResourcePatternResolver patternResolver) {
+        m_patternResolver = patternResolver;
+    }
+
     /**
      * @return Returns the path matcher that is used to interprete a specific
      *      wildcard notation.
@@ -130,13 +182,6 @@ public class ListResourcePatternResolverDecorator
      */
     public void setPathMatcher(PathMatcher pathMathcher) {
         m_pathMatcher = pathMathcher;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public Resource getResource(String location) {
-        return m_patternResolver.getResource(location);
     }
 
     /**
@@ -162,26 +207,54 @@ public class ListResourcePatternResolverDecorator
     }
 
     /**
+     * @return Returns the mostSpecificResourceLast.
+     */
+    public boolean isMostSpecificResourceLast() {
+        return m_mostSpecificResourceLast;
+    }
+
+    /**
+     * @param mostSpecificResourceLast Is the mostSpecificResourceLast to set.
+     */
+    public void setMostSpecificResourceLast(boolean mostSpecificResourceLast) {
+        m_mostSpecificResourceLast = mostSpecificResourceLast;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Resource getResource(String location) {
+        Resource resource = null;
+        if (location.startsWith(CLASSPATH_URL_PREFIX)) {
+            resource = findSingleClassPathResource(
+                location.substring(CLASSPATH_URL_PREFIX.length()));
+        } else if (location.startsWith(CLASSPATH_ALL_URL_PREFIX)) {
+            resource = findSingleClassPathResource(
+                location.substring(CLASSPATH_ALL_URL_PREFIX.length()));
+        }
+        
+        if (resource == null) { 
+            resource = delegateResourceLookup(location);
+        }
+        return resource;
+    }
+
+    /**
      * {@inheritDoc}
      */
     public Resource[] getResources(String locationPattern) throws IOException {
-        Resource[] resources;
+        Resource[] resources = null;
         if (locationPattern.startsWith(CLASSPATH_ALL_URL_PREFIX)) {
-            String locationSubPattern
-                = locationPattern.substring(CLASSPATH_ALL_URL_PREFIX.length());
+            String pattern = locationPattern.substring(
+                CLASSPATH_ALL_URL_PREFIX.length());
             
-            String[] locations;
-            
-            if (getPathMatcher().isPattern(locationSubPattern)) {
-                locations = findAllMatchingResources(locationSubPattern);
+            if (getPathMatcher().isPattern(pattern)) {
+                resources = findAllMatchingResources(pattern);
             } else {
-                locations = findAllClasspathResources(locationSubPattern);
+                resources = findAllClassPathResources(pattern);
             }
 
-            resources = resolveResourceLocations(locations);
-            
-            if (m_mergeWithOuterResources || locations.length == 0) {
-                // delegate to Spring's resource pattern resolver
+            if (isMergeWithOuterResources()) {
                 try {
                     resources = mergeResources(resources,
                         delegateResourcesLookup(locationPattern));
@@ -190,10 +263,34 @@ public class ListResourcePatternResolverDecorator
                             ioe);
                 }
             }
+        } else if (locationPattern.startsWith(CLASSPATH_URL_PREFIX)) {
+            String pattern = locationPattern.substring(
+                CLASSPATH_URL_PREFIX.length());
             
+            Resource singleResource = null;
+            if (!getPathMatcher().isPattern(pattern)) {
+                // If it is no pattern try to find the resource in
+                // in the ordered resource list.
+                singleResource = findSingleClassPathResource(pattern);
+            }
+            
+            if (singleResource != null) {
+                resources = new Resource[] {singleResource};
+            } else {
+                s_logger.warn("Ordered resource loading not supported for "
+                    + "location pattern '" + locationPattern + "'!");
+                resources = delegateResourcesLookup(locationPattern);
+            }
         } else {
             resources = delegateResourcesLookup(locationPattern);
         }
+        
+        // Reverse the array if the most specific resource should be at the
+        // beginning of the array.
+        if (!isMostSpecificResourceLast()) {
+            ArrayUtils.reverse(resources);
+        }
+        
         return resources;
     }
 
@@ -208,11 +305,25 @@ public class ListResourcePatternResolverDecorator
      * @throws IOException
      *      If an I/O error occurs.
      */
-    private Resource[] delegateResourcesLookup(String locationPattern) 
+    protected Resource[] delegateResourcesLookup(String locationPattern) 
         throws IOException {
-        
-        s_logger.info("Delegating resource lookup [" + locationPattern + "]");
+        s_logger.info("Delegating resources lookup with pattern '" 
+            + locationPattern + "'.");
         return m_patternResolver.getResources(locationPattern);
+    }
+    
+    /**
+     * Resolves the given location into one resource, using delegation.
+     * 
+     * @param location
+     *      The location to resolve.
+     *      
+     * @return Returns the resolved resource.
+     */
+    protected Resource delegateResourceLookup(String location) {
+        s_logger.info("Delegating resource lookup for location '"
+            + location + "'.");
+        return m_patternResolver.getResource(location);
     }
     
     /**
@@ -223,53 +334,123 @@ public class ListResourcePatternResolverDecorator
      *      The location pattern to match against (contains a particular
      *      wildcard notation).
      *      
-     * @return Returns a list of location names that match the given pattern.
+     * @return Returns a list of resources that matches the given pattern.
      */
-    protected String[] findAllMatchingResources(String locationPattern) {
-        List result = new ArrayList();
-        for (int i = 0; i < m_locations.length; i++) {
-            if (getPathMatcher().match(locationPattern, m_locations[i])) {
-                result.add(m_locations[i]);
+    protected Resource[] findAllMatchingResources(String locationPattern) {
+        List<Resource> result = new ArrayList<Resource>();
+        for (int i = 0; i < m_configLocations.length; i++) {
+            if (getPathMatcher().match(locationPattern, m_configLocations[i])) {
+                result.add(m_configLocationResources[i]);
             }
         }
-        return (String[]) result.toArray(new String[result.size()]);
+        return (Resource[]) result.toArray(new Resource[result.size()]);
     }
     
     /**
      * Finds all resources that do not contain any wildcards except the
      * <code>classpath*</code>.
      * 
-     * @param locationPattern
+     * @param location
      *      The location pattern to match resources against (without any
      *      particular wildcard notation).
      *      
-     * @return Returns a list of location names that match the given pattern.
+     * @return Returns a list of resources that match the given pattern.
      */
-    protected String[] findAllClasspathResources(String locationPattern) {
-        List result = new ArrayList();
-        for (int i = 0; i < m_locations.length; i++) {
-            if (locationPattern.equals(m_locations[i])) {
-                result.add(m_locations[i]);
+    protected Resource[] findAllClassPathResources(String location) {
+        List<Resource> result = new ArrayList<Resource>();
+        for (int i = 0; i < m_configLocations.length; i++) {
+            if (location.equals(m_configLocations[i])) {
+                result.add(m_configLocationResources[i]);
             }
         }
-        return (String[]) result.toArray(new String[result.size()]);
+        return (Resource[]) result.toArray(new Resource[result.size()]);
     }
     
     /**
-     * Transforms a list of location names into a list of resources.
-     * 
-     * @param locations
-     *      The list of location names.
-     *      
-     * @return Returns the list of resources referenced by the given location
-     *      names.
+     * @param location Is the looked up resource location.
+     * @return Returns only the first found class path resource.
      */
-    private Resource[] resolveResourceLocations(String[] locations) {
-        Resource[] resources = new Resource[locations.length];
-        for (int i = 0; i < locations.length; i++) {
-            resources[i] = getResource(CLASSPATH_RESOURCE + locations[i]);
+    protected Resource findSingleClassPathResource(String location) {
+        Resource resource = null;
+        try {
+            if (isMostSpecificResourceLast()) {
+                resource = findFirstClassPathResource(location);
+            } else {
+                resource = findLastClassPathResource(location);
+            }
+        } catch (IOException e) {
+            s_logger.warn("Exception occurred while lookup of single class "
+                + "path resource at location '" + location + "'.", e);
         }
-        return resources;
+        return resource;
+    }
+    
+    /**
+     * @param location Is the looked up resource location.
+     * @return Returns only the first found class path resource.
+     * @throws IOException On any io problem.
+     */
+    protected Resource findFirstClassPathResource(String location)
+        throws IOException {
+        Resource resource = null;
+        for (int i = 0; i < m_configLocations.length && resource == null; i++) {
+            if (location.equals(m_configLocations[i])) {
+                resource = m_configLocationResources[i];
+            }
+        }
+        
+        if (resource == null && isMergeWithOuterResources()) {
+            resource = delegateResourceLookup(location);
+        }
+        
+        return resource;
+    }
+    
+    /**
+     * @param location Is the looked up resource location.
+     * @return Returns only the last found class path resource. If flag
+     *         <code>mergeWithOuterResources</code> is set to <code>true</code>
+     *         this method will try to find a resource that is not in the 
+     *         config location list.
+     * @throws IOException On any io problem.
+     */
+    protected Resource findLastClassPathResource(String location)
+        throws IOException {
+        Resource resource = null;
+        Map<URL, Resource> resourceUrlMap = new LinkedHashMap<URL, Resource>();
+        if (isMergeWithOuterResources()) {
+            Resource[] resources = delegateResourcesLookup(location);
+            for (Resource r : resources) {
+                URL url = r.getURL();
+                resourceUrlMap.put(url, r);
+            }
+            
+            List<Resource> orderedResources = new ArrayList<Resource>();
+            for (int i = m_configLocations.length - 1; i >= 0; i--) {
+                if (location.equals(m_configLocations[i])) {
+                    Resource r = m_configLocationResources[i];
+                    URL url = r.getURL();
+                    resourceUrlMap.remove(url);
+                    orderedResources.add(r);
+                }
+            }
+            
+            if (resourceUrlMap.size() > 0) {
+                resource 
+                    = resourceUrlMap.entrySet().iterator().next().getValue();
+            } else if (orderedResources.size() > 0) {
+                resource = orderedResources.get(0);
+            }
+        } else {
+            for (int i = m_configLocations.length - 1; 
+                i >= 0 && resource == null; i--) {
+                if (location.equals(m_configLocations[i])) {
+                    resource = m_configLocationResources[i];
+                }
+            }
+        }
+
+        return resource;
     }
     
     /**
@@ -291,7 +472,7 @@ public class ListResourcePatternResolverDecorator
      * @throws IOException
      *      If an I/O exception occurs.
      */
-    private Resource[] mergeResources(Resource[] former, Resource[] latter)
+    protected Resource[] mergeResources(Resource[] former, Resource[] latter)
         throws IOException {
         
         if (s_logger.isDebugEnabled()) {
@@ -299,10 +480,16 @@ public class ListResourcePatternResolverDecorator
             debugArray("latter", latter);
         }
         
-        ArrayList result = new ArrayList();
-        for (int i = 0; i < latter.length; i++) {
-            if (!result.contains(latter[i])) {
-                result.add(latter[i]);
+        List<Resource> result = new ArrayList<Resource>();
+        List<URL> urlList = new ArrayList<URL>();
+        for (Resource resource : former) {
+            result.add(resource);
+            urlList.add(resource.getURL());
+        }
+        for (Resource resource : latter) {
+            URL url = resource.getURL();
+            if (!urlList.contains(url)) {
+                result.add(resource);
             }
         }
         return (Resource[]) result.toArray(new Resource[result.size()]);
@@ -317,7 +504,7 @@ public class ListResourcePatternResolverDecorator
      * @param array
      *      The array to debug.
      */
-    private void debugArray(String msg, Object[] array) {
+    protected void debugArray(String msg, Object[] array) {
         String s = StringUtils.arrayToCommaDelimitedString(array);
         s_logger.debug(msg + " [" + s + "]");
     }
