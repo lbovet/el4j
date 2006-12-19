84f4f95647056242c754fe4d3e460a1f777ecd6e
enylaios <enylaios@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2006-12-19 14:46:10 +0000

    Reverted changes made to plugin

--- trunk/el4j/plugins/maven-database-plugin/src/main/java/ch/elca/el4j/plugins/database/mojo/DropMojo.java
+++ trunk/el4j/plugins/maven-database-plugin/src/main/java/ch/elca/el4j/plugins/database/mojo/DropMojo.java
@@ -49,7 +49,7 @@ public class DropMojo extends AbstractDBMojo {
         try {
             executeAction(ACTION);
         } catch (Exception e) {
-            throw new MojoExecutionException(e.getMessage());
+            getLog().error("Error during drop", e);
         }
     }
 }
