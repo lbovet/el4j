30fcf39a231ff9c4ce693219d25b5929e2c22198
enylaios <enylaios@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2007-01-09 13:14:28 +0000

    Worked on prototype

--- el4j/sandbox/addressbook/src/main/resources/etc/sql/update-contacts.sql
+++ el4j/sandbox/addressbook/src/main/resources/etc/sql/update-contacts.sql
@@ -1,3 +1,8 @@
 INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city) VALUES ('Arnold', 'Schwarzenegger', '10921 The Other Street', 'Los Angeles');
+INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city) VALUES ('Donald', 'Duck', 'Disney Drive.', 'Fort Wayne');
+INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city) VALUES ('Albert', 'Einstein', '279 Princeton Way', 'Princeton');
 INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city) VALUES ('Silvester', 'Stalone', 'Apt. 12D', 'Chicago');
-INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city) VALUES ('Vin', 'Diesel', '456 WebFlow Rd.', 'Denver')
+INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city) VALUES ('Jan', 'Vermeer', 'Daagster 72', 'Antwerpen');
+INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city) VALUES ('Bill', 'Gates', '777 Ms Lane', 'Redmond');
+INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city) VALUES ('Vin', 'Diesel', 'n33rd WebFlow Rd.', 'Denver');
+INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city) VALUES ('Marie', 'Curie', '101 Potsdamer Platz .', 'Berlin');
