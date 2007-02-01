767a42df2c25c1dc8c212ede6fa216f14e717792
enylaios <enylaios@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2007-02-01 12:37:31 +0000

    Updated spring rcp v.0.3 work

--- el4j/sandbox/addressbook/src/main/resources/etc/sql/update-contacts.sql
+++
@@ -1,8 +0,0 @@
-INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city, optimisticLockingVersion) VALUES ('Arnold', 'Schwarzenegger', '10921 The Other Street', 'Los Angeles', 0);
-INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city, optimisticLockingVersion) VALUES ('Donald', 'Duck', 'Disney Drive.', 'Fort Wayne', 1);
-INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city, optimisticLockingVersion) VALUES ('Albert', 'Einstein', '279 Princeton Way', 'Princeton', 2);
-INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city, optimisticLockingVersion) VALUES ('Silvester', 'Stalone', 'Apt. 12D', 'Chicago', 3);
-INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city, optimisticLockingVersion) VALUES ('Jan', 'Vermeer', 'Daagster 72', 'Antwerpen', 4);
-INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city, optimisticLockingVersion) VALUES ('Bill', 'Gates', '777 Ms Lane', 'Redmond', 5);
-INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city, optimisticLockingVersion) VALUES ('Vin', 'Diesel', 'n33rd WebFlow Rd.', 'Denver', 6);
-INSERT into CONTACTS (FIRSTNAME, LASTNAME, address, city, optimisticLockingVersion) VALUES ('Marie', 'Curie', '101 Potsdamer Platz .', 'Berlin', 7);
