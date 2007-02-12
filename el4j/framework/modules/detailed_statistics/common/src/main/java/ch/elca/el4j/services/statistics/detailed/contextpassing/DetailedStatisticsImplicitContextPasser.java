385c62051468e5cc6c761bc78de53ccdce794177
poser55 <poser55@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2007-02-12 13:41:44 +0000

    fixed issues with the detailed statistics implementation

---
+++ el4j/framework/modules/detailed_statistics/common/src/main/java/ch/elca/el4j/services/statistics/detailed/contextpassing/DetailedStatisticsImplicitContextPasser.java
@@ -0,0 +1,64 @@
+/*
+ * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
+ * the spring framework, http://el4j.sf.net
+ * Copyright (C) 2005 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * EL4J is published under the GNU Lesser General Public License (LGPL)
+ * Version 2.1. See http://www.gnu.org/licenses/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+package ch.elca.el4j.services.statistics.detailed.contextpassing;
+
+
+import ch.elca.el4j.core.contextpassing.AbstractImplicitContextPasser;
+import ch.elca.el4j.core.contextpassing.ImplicitContextPassingRegistry;
+/**
+ * 
+ * This ImplicitContextPasser passes the DetailedStatisticsSharedContextHolder as implicit context.
+ *
+ * <script type="text/javascript">printFileStatus
+ *   ("$URL$",
+ *    "$Revision$",
+ *    "$Date$",
+ *    "$Author$"
+ * );</script>
+ *
+ * @author Rashid Waraich (RWA)
+ */
+public class DetailedStatisticsImplicitContextPasser 
+    extends AbstractImplicitContextPasser {
+    
+    /**
+     * The key to the shared context.
+     */
+    public static final String SHARED_CONTEXT_ID = "detailedStatistics";
+
+    /**
+     * {@inheritDoc}
+     */
+    public Object getImplicitlyPassedContext() {
+        return DetailedStatisticsSharedContextHolder.getContext();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void pushImplicitlyPassedContext(Object context) {
+        DetailedStatisticsSharedContextHolder.setContext(((DetailedStatisticsContext)context));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setImplicitContextPassingRegistry(
+        ImplicitContextPassingRegistry registry) {
+        super.setImplicitContextPassingRegistry(registry);
+    }
+}
---
+++ el4j/framework/modules/detailed_statistics/common/src/main/java/ch/elca/el4j/services/statistics/detailed/contextpassing/DetailedStatisticsSharedContextHolder.java
@@ -0,0 +1,64 @@
+/*
+ * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
+ * the spring framework, http://el4j.sf.net
+ * Copyright (C) 2005 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * EL4J is published under the GNU Lesser General Public License (LGPL)
+ * Version 2.1. See http://www.gnu.org/licenses/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+package ch.elca.el4j.services.statistics.detailed.contextpassing;
+
+/**
+ * 
+ * This class is specific implementation of the generic shared context 
+ * of Leaf 2 for the detailed statistics service. The shared context is 
+ * held in a static ThreadLocal variable.
+ *
+ * <script type="text/javascript">printFileStatus
+ *   ("$URL$",
+ *    "$Revision$",
+ *    "$Date$",
+ *    "$Author$"
+ * );</script>
+ *
+ * @author Rashid Waraich (RWA)
+ */
+public class DetailedStatisticsSharedContextHolder {
+    
+    /** 
+     * Internal holder of thread static variable
+     */
+    private static final ThreadLocal<DetailedStatisticsContext> 
+             s_detailedStatisticsContext = new ThreadLocal<DetailedStatisticsContext>() {
+        @Override
+        protected DetailedStatisticsContext initialValue() {
+            return new DetailedStatisticsContext();
+        }
+    };    
+    
+    /**
+     * Get the passed object.
+     * 
+     * @return DetailedStatisticsContext.
+     */
+    public static final DetailedStatisticsContext getContext() {
+        return s_detailedStatisticsContext.get();
+    }
+   
+    /**
+     * Set the passed object.
+     * @param passedObject The passed object.
+     */
+    public static final void setContext(DetailedStatisticsContext passedObject) {
+        s_detailedStatisticsContext.set(passedObject);
+    }
+
+}
