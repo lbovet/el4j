09f066d170b06156be5cc17947c91c2830fb4b0f
enylaios <enylaios@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2007-02-13 08:05:06 +0000

    Updated spring rcp v.0.3 work

---
+++ trunk/el4j/sandbox/framework/apps/refdb/gui-tiger/thin-client/src/main/java/ch/elca/el4j/services/richclient/config/Edit.java
@@ -0,0 +1,300 @@
+/*
+ * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
+ * the spring framework, http://el4j.sf.net
+ * Copyright (C) 2006 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * EL4J is published under the GNU Lesser General Public License (LGPL)
+ * Version 2.1. See http://www.gnu.org/licenses/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+package ch.elca.el4j.services.richclient.config;
+
+import javax.swing.JComponent;
+
+import org.springframework.binding.form.FormModel;
+import org.springframework.binding.form.support.AbstractFormModel;
+import org.springframework.binding.form.support.DefaultFieldMetadata;
+import org.springframework.richclient.application.ViewDescriptor;
+import org.springframework.richclient.dialog.DialogPage;
+
+import ch.elca.el4j.services.gui.richclient.executors.convenience.AbstractBeanPropertiesExecutor;
+import ch.elca.el4j.services.gui.richclient.executors.displayable.ExecutorDisplayable;
+import ch.elca.el4j.services.gui.richclient.forms.BeanPropertiesForm;
+import ch.elca.el4j.services.gui.richclient.utils.Services;
+import ch.elca.el4j.services.gui.richclient.views.DialogPageView;
+import ch.elca.el4j.services.i18n.SimpleFieldFaceSource;
+import ch.elca.el4j.services.persistence.generic.DaoAgency;
+import ch.elca.el4j.services.persistence.generic.dao.ConvenientGenericDao;
+import ch.elca.el4j.services.persistence.generic.dao.GenericDao;
+import ch.elca.el4j.services.persistence.generic.dto.PrimaryKeyObject;
+import ch.elca.el4j.util.codingsupport.annotations.ImplementationAssumption;
+import ch.elca.el4j.util.codingsupport.annotations.Preliminary;
+import ch.elca.el4j.util.collections.ExtendedList;
+import ch.elca.el4j.util.dom.reflect.Property;
+import ch.elca.el4j.util.observer.ObservableValue;
+import ch.elca.el4j.util.observer.ValueObserver;
+import ch.elca.el4j.util.observer.impl.SettableObservableValue;
+
+
+
+
+
+
+/**
+ * a form showing a specific bean's properties, supporting their editing.
+ *
+ * <script type="text/javascript">printFileStatus
+ *   ("$URL$",
+ *    "$Revision$",
+ *    "$Date$",
+ *    "$Author$"
+ * );</script>
+ *
+ * @author Adrian Moos (AMS)
+ */
+public class Edit extends AbstractGenericView {
+    /** specifies which properties are shown and which are changeable. */
+    public EditablePropertyList properties;
+    
+    /** holds the domain object currently being edited. */
+    public ObservableValue<?> current;
+
+    /** the backing executor. */
+    Executor m_executor;
+    
+    /** the list of visible properties. */
+    ExtendedList<EditableProperty> m_visible;
+
+    /**
+     * creates a new edit view for entities represented by (a subtype of)
+     * {@code c}.
+     * @param c .
+     */
+    public Edit(Class<?> c) {
+        super(c);
+        properties = new EditablePropertyList(m_type);
+        current = new SettableObservableValue<Object>(null);
+    }
+    
+    /***/
+    class Executor<T> extends AbstractBeanPropertiesExecutor {
+        ///////////////////////
+        // persistence logic
+        ///////////////////////
+        
+        /** The repository to be used. */
+        GenericDao<T> m_repository;
+
+        /**
+         * {@inheritDoc}
+         */
+        @SuppressWarnings("unchecked")
+        @Override
+        protected PrimaryKeyObject saveBean(PrimaryKeyObject givenBean) {
+            return (PrimaryKeyObject) m_repository.saveOrUpdate((T) givenBean);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        protected PrimaryKeyObject getBeanByKey(Object key) throws Exception {
+            return (PrimaryKeyObject) 
+                ((ConvenientGenericDao<T, Integer>) m_repository)
+                    .findById((Integer) key);
+        }
+        
+        /**
+         * {@inheritDoc}
+         */
+        @Override public
+        boolean onFinishAfterCommit(Object currentBean) throws Exception {
+            PrimaryKeyObject givenBean = (PrimaryKeyObject) currentBean;
+            PrimaryKeyObject savedBean = saveBean(givenBean);
+            FormModel formModel = getFormModel();
+            formModel.setFormObject(savedBean);
+            return true;
+        }
+        
+        
+        ///////////////////
+        // execution
+        ///////////////////
+        
+        /** executes on the supplied bean. 
+         * @see AbstractBeanPropertiesExecutor#execute() */
+        // largely copied from super.execute(), but modified to use the supplied
+        // bean or pull one out of the hat
+        @ImplementationAssumption(
+            "every dom type can be instantiated using a default constructor")
+        protected void execute(Object bean) {
+            FormModel formModel = getFormModel();
+            DialogPage dialogPage = getDialogPage();
+            if (formModel == null || dialogPage == null) {
+                // Kludge: FormModels must be configured using instances,
+                // not classes. We therefore create a temporary instance.
+                Object exampleBean = bean;
+                if (exampleBean == null) {
+                    try {
+                        exampleBean = m_type.clazz.newInstance();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                initializeFormModelAndDialogPage(exampleBean);
+            } else {
+                formModel.setFormObject(bean);
+            }
+            
+            ExecutorDisplayable displayable = getDisplayable();
+            if (!displayable.isConfigured()) {
+                displayable.configure(this);
+            }
+        }
+
+        
+        ///////////////////
+        // temporary fixes
+        ///////////////////
+        
+        @Override
+        public String getSchema() {
+            return m_type.name;
+        }
+
+        Executor() {
+            setId(m_type.name + "Properties");
+        }
+
+        
+        ///////////////////
+        // localization
+        ///////////////////
+        
+        /**
+         * {@inheritDoc}
+         **/
+        @Override @Preliminary @ImplementationAssumption(
+            "All form models are subtypes of AbstractFormModel. "
+            + "Instances of FieldMetadata are always DefaultFieldMetadata")
+        protected void initializeFormModelAndDialogPage(Object bean) {
+            super.initializeFormModelAndDialogPage(bean);
+            // HACK: implementation dependent downcast. Spring RCP's public API
+            // does not permit to configure the localization provider used.
+            // Change request submitted to Spring RCP team pending, see
+            // http://opensource.atlassian.com/projects/spring/browse/RCP-389
+            AbstractFormModel fm = (AbstractFormModel) getFormModel();
+            fm.setFieldFaceSource(
+                SimpleFieldFaceSource.instance()
+            );
+
+            for (DisplayableProperty ep : m_visible) {
+                // HACK: Implementation dependent downcast.
+                //       The public api does not permit to set user metadata
+                //       for an already created form model.
+                // This should be done by providing a property metadata access
+                // strategy to maintain propert initialization order.
+                // (getting field metadata already creates the value model)
+                DefaultFieldMetadata metadata 
+                    = ((DefaultFieldMetadata) 
+                        fm.getFieldMetadata(ep.prop.name));
+                metadata.setUserMetadata(
+                    AbstractGenericView.class.getName(),
+                    Edit.this
+                );
+                metadata.setUserMetadata(
+                    Property.class.getName(),
+                    ep.prop
+                );
+            }
+        }
+    }
+
+    /***/
+    class GenericComponent extends DialogPageView 
+                        implements ValueObserver<Object> {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        protected JComponent createControl() {
+            current.subscribe(this);
+            return super.createControl();
+        }
+        
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void componentOpened() {
+            super.componentOpened();
+            updateControl();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void dispose() {
+            current.unsubscribe(this);
+            super.dispose();
+        }
+
+        /**
+         * updates the component to reflect the current domain object.
+         * @param newValue .
+         */
+        public void changed(Object newValue) {
+            m_executor.execute(newValue);
+            updateControl();
+        }
+
+        /** 
+         * updates the control to reflect the current domain object 
+         * (if the control has already been created). 
+         */
+        protected void updateControl() {
+            if (isControlCreated()) {
+                // possibly just disable? Is not trivial though, as enabled
+                // does not propagate to child components
+                getControl().setVisible(current.get() != null);
+            }
+        }
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected <T> ViewDescriptor createDescriptor(Class<T> clazz) {
+        m_visible = properties.m_eprops
+            .filtered(DisplayablePropertyList.s_visibles);
+
+        BeanPropertiesForm form = new BeanPropertiesForm();
+        form.setShownBeanProperties(
+            m_visible.mapped(
+                DisplayablePropertyList.s_toName
+            ).toArray(String.class)
+        );
+        form.setReadOnlyBeanProperties(properties.getReadonly());
+        m_awaker.awaken(form);
+        
+        GenericComponent gc = new GenericComponent();
+        Executor<T> exec = new Executor<T>(); 
+        exec.m_repository = Services.get(DaoAgency.class)
+                                    .getFor(clazz);
+        exec.setBeanPropertiesForms(new BeanPropertiesForm[] {form});
+        exec.setDisplayable(gc);
+        m_awaker.awaken(exec);
+        m_executor = exec;
+        
+        return configure(new Descriptor(gc));
+    }
+}
