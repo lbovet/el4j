30424da192d55488eff11849f738fb81cbfafd10
poser55 <poser55@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2007-07-22 20:37:17 +0000

    added configs for mysql

--- trunk/el4j/applications/templates/common/refdb/dao/src/main/resources/etc/sql/oracle/create-reference.sql
+++ trunk/el4j/applications/templates/common/refdb/dao/src/main/resources/etc/sql/oracle/create-reference.sql
@@ -3,19 +3,15 @@
 -- Date: $Date$
 -- Author: $Author$
 
-CREATE SEQUENCE reference_sequence    INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE annotation_sequence   INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE file_sequence         INCREMENT BY 1 START WITH 1;
-
 CREATE TABLE referencesTable (
-  keyId                     INTEGER        
-  	NOT NULL PRIMARY KEY,
+  keyId                     INTEGER        NOT NULL 
+    PRIMARY KEY AUTO_INCREMENT,
   name                      VARCHAR(64)    NOT NULL,
   hashValue                 VARCHAR(64),
   description               VARCHAR(256),
   version                   VARCHAR(64),
   incomplete                DECIMAL(1),
-  whenInserted              TIMESTAMP      NOT NULL,
+  whenInserted              DATETIME      NOT NULL,
   documentDate              DATE,
   optimisticLockingVersion  INTEGER        NOT NULL
 );
@@ -31,7 +27,7 @@ CREATE TABLE formalPublications (
   	NOT NULL REFERENCES referencesTable(keyId),
   authorName                VARCHAR(64),
   publisher                 VARCHAR(64),
-  pageNum                   NUMBER(4)
+  pageNum                   SMALLINT
 );
 
 CREATE TABLE books (
@@ -39,7 +35,7 @@ CREATE TABLE books (
   	NOT NULL REFERENCES referencesTable(keyId),
   authorName                VARCHAR(64),
   publisher                 VARCHAR(64),
-  pageNum                   NUMBER(4),
+  pageNum                   SMALLINT,
   isbnNumber                VARCHAR(20)
 );
 
@@ -52,25 +48,25 @@ CREATE TABLE referenceKeywordRelationships (
 );
 
 CREATE TABLE annotations (
-  keyId                     INTEGER        
-  	NOT NULL PRIMARY KEY,
+  keyId                     INTEGER        NOT NULL
+    PRIMARY KEY AUTO_INCREMENT,
   keyToReference            INTEGER        
   	NOT NULL REFERENCES referencesTable(keyId),
   annotator                 VARCHAR(64)    NOT NULL,
   grade                     DECIMAL(2)     NOT NULL,
-  content                   CLOB           NOT NULL,
-  whenInserted              TIMESTAMP      NOT NULL,
+  content                   LONGBLOB       NOT NULL,
+  whenInserted              DATETIME      NOT NULL,
   optimisticLockingVersion  INTEGER        NOT NULL
 );
 
 CREATE TABLE files (
-  keyId                     INTEGER        
-  	NOT NULL PRIMARY KEY,
+  keyId                     INTEGER        NOT NULL
+    PRIMARY KEY AUTO_INCREMENT,
   keyToReference            INTEGER        
   	NOT NULL REFERENCES referencesTable(keyId),
   name                      VARCHAR(64)    NOT NULL,
   mimeType                  VARCHAR(32)    NOT NULL,
-  contentSize               NUMBER(10)     NOT NULL,
-  content                   BLOB           NOT NULL,
+  contentSize               DOUBLE         NOT NULL,
+  content                   LONGBLOB       NOT NULL,
   optimisticLockingVersion  INTEGER        NOT NULL
 );
