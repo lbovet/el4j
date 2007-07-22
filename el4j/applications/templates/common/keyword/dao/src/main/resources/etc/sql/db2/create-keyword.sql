d3339e4541e02816f5ee20682ed92477a5c6ac9b
poser55 <poser55@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2007-07-22 21:56:57 +0000

    undone earlier wrong change

--- el4j/applications/templates/common/keyword/dao/src/main/resources/etc/sql/db2/create-keyword.sql
+++ el4j/applications/templates/common/keyword/dao/src/main/resources/etc/sql/db2/create-keyword.sql
@@ -4,8 +4,11 @@
 -- Author: $Author$
 
 CREATE TABLE keywords (
-  keyId                     INTEGER        NOT NULL    PRIMARY KEY AUTO_INCREMENT,
+  keyId                     INTEGER        NOT NULL 
+    GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)
+    PRIMARY KEY,
   name                      VARCHAR(64)    UNIQUE NOT NULL,
   description               VARCHAR(256),
   optimisticLockingVersion  INTEGER        NOT NULL
 );
+
