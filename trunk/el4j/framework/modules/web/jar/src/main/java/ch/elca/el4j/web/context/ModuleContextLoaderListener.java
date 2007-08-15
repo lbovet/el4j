43ad7f55440286480da3d5bf00c44ce2d914f4f0
cba7 <cba7@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2007-08-15 15:30:59 +0000

    documented web.xml params for ModuleWebApplicationContext

--- trunk/el4j/framework/modules/web/jar/src/main/java/ch/elca/el4j/web/context/ModuleContextLoaderListener.java
+++ trunk/el4j/framework/modules/web/jar/src/main/java/ch/elca/el4j/web/context/ModuleContextLoaderListener.java
@@ -30,6 +30,49 @@ import org.springframework.web.context.ContextLoaderListener;
  *    "$Author$"
  * );</script>
  *
+ * Sample Configuration in web.xml:
+ * 
+ *  &lt;context-param&gt;
+ *       &lt;description&gt;
+ *           Configuration for the EL4J Application Context
+ *       &lt;/description&gt;
+ *       &lt;param-name&gt;contextConfigLocation&lt;/param-name&gt;
+ *       &lt;param-value&gt;
+ *           classpath*:mandatory/*.xml,
+ *           classpath*:mandatory/generic/*.xml,
+ *           classpath*:scenarios/db/raw/*.xml,
+ *           classpath*:scenarios/dataaccess/hibernate/*.xml,
+ *           classpath*:scenarios/dataaccess/hibernate/keyword/*.xml,
+ *           classpath*:optional/interception/transactionJava5Annotations.xml
+ *       &lt;/param-value&gt;
+ *   &lt;/context-param&gt;
+ *   
+ *   &lt;context-param&gt;
+ *       &lt;description&gt;
+ *           Configuration locations to exclude from the application context.
+ *       &lt;/description&gt;
+ *       &lt;param-name&gt;exclusiveLocations&lt;/param-name&gt;
+ *       &lt;param-value&gt;
+ *           mandatory/keyword/keyword-core-config.xml
+ *       &lt;/param-value&gt;
+ *   &lt;/context-param&gt;
+ *   
+ *   &lt;context-param&gt;
+ *       &lt;description&gt;
+ *           Bean definition overriding allowed in the application context?
+ *       &lt;/description&gt;
+ *       &lt;param-name&gt;overrideBeanDefinitions&lt;/param-name&gt;
+ *       &lt;param-value&gt;
+ *           true
+ *       &lt;/param-value&gt;
+ *   &lt;/context-param&gt;
+ *   
+ *   &lt;listener&gt;
+ *       &lt;listener-class&gt;
+ *           ch.elca.el4j.web.context.ModuleContextLoaderListener
+ *       &lt;/listener-class&gt;
+ *   &lt;/listener&gt;
+ *
  * @see org.springframework.web.context.ContextLoaderListener
  * @see ModuleContextLoader
  * @author Alex Mathey (AMA)
