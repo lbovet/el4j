186dbdb5c9c445e524bb703bfa24b5615fcb2a79
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2007-08-15 09:12:55 +0000

    Tagging the EL4J release 1.1.3

---
+++ tags/el4j_1_1_3/el4j/applications/demos/remoting/web/jar/src/main/java/ch/elca/el4j/demos/remoting/Calculator.java
@@ -0,0 +1,73 @@
+/*
+ * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
+ * the spring framework, http://el4j.sf.net
+ * Copyright (C) 2005 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * EL4J is published under the GNU Lesser General Public License (LGPL)
+ * Version 2.1. See http://www.gnu.org/licenses/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+
+package ch.elca.el4j.demos.remoting;
+
+
+
+/**
+ * This interface is a calculator.
+ *
+ * <script type="text/javascript">printFileStatus
+ *   ("$URL$",
+ *    "$Revision$",
+ *    "$Date$",
+ *    "$Author$"
+ * );</script>
+ *
+ * @author Martin Zeltner (MZE)
+ * @author Rashid Waraich (RWA)
+ */
+
+public interface Calculator {
+    /**
+     * This method calculates the area of a rectangle.
+     * 
+     * @param a
+     *            Is the first side.
+     * @param b
+     *            Is the second side.
+     * @return Returns the area of the triangle.
+     */
+    public double getArea(double a, double b);
+    
+    /**
+     * This method throws an exception for test reason.
+     * 
+     * @throws CalculatorException will be thrown every time.
+     */
+    public void throwMeAnException() throws CalculatorException;
+    
+    /**
+     * This method counts all uppercase letters of a text. 
+     * 
+     * @param text Is the object to analyze.
+     * @return Returns the number of uppercase letters.
+     */
+    public int countNumberOfUppercaseLetters(String text);
+    
+    /**
+     * This method adds two complex numbers.
+     * 
+     * @param cn1
+     *              The first ComplexNumber
+     * @param cn2
+     *              The second ComplexNumber
+     * @return Returns the sum of the ComplexNumbers
+     */
+    public ComplexNumber add(ComplexNumber cn1, ComplexNumber cn2);
+}
---
+++ tags/el4j_1_1_3/el4j/sandbox/old-el4j/framework/modules/remoting_ejb/src/test/java/ch/elca/el4j/tests/remoting/ejb/service/impl/LibraryImpl.java
@@ -0,0 +1,154 @@
+/*
+ * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
+ * the spring framework, http://el4j.sf.net
+ * Copyright (C) 2005 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * EL4J is published under the GNU General Public License (GPL) Version 2.0.
+ * http://www.gnu.org/licenses/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+
+package ch.elca.el4j.tests.remoting.ejb.service.impl;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.ejb.SessionContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import ch.elca.el4j.tests.remoting.ejb.service.Library;
+
+/**
+ * Library implementation used for testing.
+ *
+ * <script type="text/javascript">printFileStatus
+ *   ("$URL$",
+ *    "$Revision$",
+ *    "$Date$",
+ *    "$Author$"
+ * );</script>
+ *
+ * @author Andreas Bur (ABU)
+ */
+public class LibraryImpl implements Library, Serializable {
+
+    /** An offset to prevent collisions. */
+    private static final int OFFSET = 100;
+    
+    /** The static logger instance. */
+    private static Log s_logger = LogFactory.getLog(LibraryImpl.class);
+    
+    /** The map wit all books. */
+    private HashMap m_books = new HashMap();
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void init(Object[] objs) {
+        s_logger.info("create(Object[]) called");
+        for (int i = 0; i < objs.length; i++) {
+            putBook(i + OFFSET, objs[i].toString());
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean putBook(int id, String bookTitle) {
+        Object obj = m_books.put(new Integer(id), bookTitle);
+        return (obj == null ? true : false);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getBookTitle(int id) {
+        return (String) m_books.get(new Integer(id));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getSize() {
+        s_logger.info("getSize()");
+        return m_books.size();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public String[] getAllBooks() {
+        return (String[]) m_books.values().toArray(new String[m_books.size()]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void putAllBooks(Map books) {
+        s_logger.info("add all books");
+        for (Iterator iter = books.entrySet().iterator(); iter.hasNext();) {
+            Map.Entry next = (Map.Entry) iter.next();
+            this.m_books.put(next.getKey(), next.getValue());
+        }
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void activate() {
+        s_logger.info("activate()");
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void beanAfterBegin() {
+        s_logger.info("beanAfterBegin()");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void beanAfterCompletion(boolean commited) {
+        s_logger.info("beanAfterCompletion(boolean), with value " + commited);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void beanBeforeCompletion() {
+        s_logger.info("beanBeforeCompletion()");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void passivate() {
+        s_logger.info("passivate()");
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void setBeanSessionContext(SessionContext context) {
+        s_logger.info("setSessionCotnext(" + context + ")");
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void removeBean() {
+        s_logger.info("removeBean called");
+    }
+}
