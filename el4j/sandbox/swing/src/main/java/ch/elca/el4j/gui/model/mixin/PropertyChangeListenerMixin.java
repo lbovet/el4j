527caf5cac90ec49dc2d5c46b0fe6f6fdc5c366e
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2007-10-10 09:09:49 +0000

    cleanup and javadoc

--- el4j/sandbox/swing/src/main/java/ch/elca/el4j/gui/model/mixin/PropertyChangeListenerMixin.java
+++ el4j/sandbox/swing/src/main/java/ch/elca/el4j/gui/model/mixin/PropertyChangeListenerMixin.java
@@ -11,6 +11,8 @@ import org.aopalliance.intercept.MethodInvocation;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.validator.ClassValidator;
+import org.jdesktop.observablecollections.ObservableCollections;
+import org.jdesktop.observablecollections.ObservableList;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.IntroductionAdvisor;
 import org.springframework.aop.framework.AopContext;
@@ -25,88 +27,103 @@ import com.silvermindsoftware.hitch.events.PropertyChangeListenerCapability;
 import com.silvermindsoftware.hitch.validation.ValidationCapability;
 
 /**
- * Mixin to enable javaBeans event support and validation in ordinary POJOs
+ * Mixin to enable javaBeans event support and validation in ordinary POJOs.
  * 
- *  To see each fired property change, 
- *   set the log4j level for this class to debug
+ * To see each fired property change, set the log4j level for this class to
+ * debug
  * 
  * @author SWI
  */
-public class PropertyChangeListenerMixin extends DelegatingIntroductionInterceptor
-    implements PropertyChangeListenerCapability, ValidationCapability {
+public class PropertyChangeListenerMixin extends
+        DelegatingIntroductionInterceptor implements
+        PropertyChangeListenerCapability, ValidationCapability {
+    
+    /**
+     * The support for property change notification.
+     */
+    private PropertyChangeSupport m_changeSupport;
+    
+    
+    /**
+     * Getter-to-setter/setter-to-getter method cache.
+     */
+    private Map<Method, Method> m_methodCache = new HashMap<Method, Method>();
 
-    private static Log s_logger = LogFactory.getLog(PropertyChangeListenerMixin.class);
-	
-	/**
-	 *  Wrap an object with the change tracking mixin.
-	 * @param object
-	 * @return the same object wrapped with a spring proxy that has the {@link PropertyChangeListenerMixin} 
-	 *   as {@link Advisor}
-	 */
-	public static Object addPropertyChangeMixin(Object object) {
-		ProxyFactory pc = new ProxyFactory (object);
-        IntroductionAdvisor ii = new DefaultIntroductionAdvisor(new PropertyChangeListenerMixin());
-		pc.setProxyTargetClass(true);
-		pc.setExposeProxy(true);
+    /**
+     * Hibernate class validator.
+     */
+    private ClassValidator m_classValidator;
+
+    private static Log s_logger = LogFactory
+            .getLog(PropertyChangeListenerMixin.class);
+
+    /** {@inheritDoc} */
+    public PropertyChangeListenerMixin() {
+        // initialize later when reference to the model is known
+        m_changeSupport = null;
+        m_classValidator = null;
+    }
+    
+    /**
+     * Wrap an object with the change tracking mixin.
+     * 
+     * @param object   the object to be wrapped
+     * @return the same object wrapped with a spring proxy that has the
+     *         {@link PropertyChangeListenerMixin} as {@link Advisor}
+     */
+    public static Object addPropertyChangeMixin(Object object) {
+        ProxyFactory pc = new ProxyFactory(object);
+        IntroductionAdvisor ii = new DefaultIntroductionAdvisor(
+                new PropertyChangeListenerMixin());
+        pc.setProxyTargetClass(true);
+        pc.setExposeProxy(true);
         pc.addAdvisor(0, ii);
         object = pc.getProxy();
-		return object;
-	}
-    
-    private PropertyChangeSupport changeSupport;
-    private Map<Method, Method> methodCache = new HashMap<Method, Method>();
-    
-    private ClassValidator classValidator;
-    
-
-	public PropertyChangeListenerMixin() {
-        // initialize later when reference to the model is known
-        changeSupport = null;
-        classValidator = null;
-    }
-    
-    public void addPropertyChangeListener(PropertyChangeListener l)
-    {
-        changeSupport.addPropertyChangeListener(l);
+        return object;
     }
 
-    public void removePropertyChangeListener(PropertyChangeListener l)
-    {
-        changeSupport.removePropertyChangeListener(l);
+    /** {@inheritDoc} */
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        m_changeSupport.addPropertyChangeListener(l);
     }
 
-    public void addPropertyChangeListener(String key, PropertyChangeListener l)
-    {
-        changeSupport.addPropertyChangeListener(key, l);
+    /** {@inheritDoc} */
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        m_changeSupport.removePropertyChangeListener(l);
     }
 
-    public void removePropertyChangeListener(String key, PropertyChangeListener l)
-    {
-        changeSupport.removePropertyChangeListener(key, l);
+    /** {@inheritDoc} */
+    public void addPropertyChangeListener(String key, PropertyChangeListener l) {
+        m_changeSupport.addPropertyChangeListener(key, l);
     }
 
-    protected void firePropertyChange(String key, Object oldValue, Object newValue)
-    {
+    /** {@inheritDoc} */
+    public void removePropertyChangeListener(String key,
+            PropertyChangeListener l) {
+        m_changeSupport.removePropertyChangeListener(key, l);
+    }
+
+    /** {@inheritDoc} */
+    protected void firePropertyChange(String key, Object oldValue,
+            Object newValue) {
         s_logger.debug("Fire " + key + ": " + oldValue + " -> " + newValue);
-        changeSupport.firePropertyChange(key, oldValue, newValue);
+        m_changeSupport.firePropertyChange(key, oldValue, newValue);
     }
 
-    
+    /** {@inheritDoc} */
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
         // initialize PropertyChangeSupport here
-        if (changeSupport == null) {
-            changeSupport = new PropertyChangeSupport(AopContext.currentProxy());
+        if (m_changeSupport == null) {
+            m_changeSupport = new PropertyChangeSupport(AopContext.currentProxy());
         }
-        if (classValidator == null) {
-            classValidator = new ClassValidator(invocation.getThis().getClass());
+        if (m_classValidator == null) {
+            m_classValidator = new ClassValidator(invocation.getThis().getClass());
         }
-        
-        // TODO delete sysout
-        //System.out.println(invocation.getMethod().getName() + " called!");
+
         if (invocation.getMethod().getName().startsWith("set")) {
             if (invocation.getArguments().length == 1) {
-                
+
                 // invoke the corresponding get method to see
                 // if the value has actually changed
                 Method getter = getGetter(invocation.getMethod());
@@ -116,25 +133,30 @@ public class PropertyChangeListenerMixin extends DelegatingIntroductionIntercept
                     // for write only methods
                     Object oldVal = getter.invoke(invocation.getThis());
                     Object result = super.invoke(invocation);
-                    
-                    String fieldName = invocation.getMethod().getName().substring(3);
-                    fieldName = fieldName.substring(0, 1).toLowerCase() + fieldName.substring(1);
-                    
-                    firePropertyChange(fieldName, oldVal, getter.invoke(invocation.getThis()));
+
+                    String fieldName = invocation.getMethod().getName()
+                            .substring(3);
+                    fieldName = fieldName.substring(0, 1).toLowerCase()
+                            + fieldName.substring(1);
+
+                    firePropertyChange(fieldName, oldVal, getter
+                            .invoke(invocation.getThis()));
                     return result;
                 }
-                throw new IllegalArgumentException("Mixin Interceptor is not able to find getter Method");
+                throw new IllegalArgumentException(
+                        "Mixin Interceptor is not able to find getter Method");
             } else {
-                throw new IllegalArgumentException("Too many arguments for Interceptor");
+                throw new IllegalArgumentException(
+                        "Too many arguments for Interceptor");
             }
         } else if (invocation.getMethod().getName().startsWith("get")
-                && List.class.isAssignableFrom(invocation.getMethod().getReturnType())) {
-            
-            /* ATTENTION Beans Binding doesn't work properly with lists/tables */
-            List result = (List)super.invoke(invocation);
-            if (!(result instanceof EventList)) {
-                // replace list by glazedList
-                result = GlazedLists.eventList(result);
+                && List.class.isAssignableFrom(invocation.getMethod()
+                        .getReturnType())) {
+
+            List result = (List) super.invoke(invocation);
+            if (!(result instanceof ObservableList)) {
+                // replace list by ObservableList
+                result = ObservableCollections.observableList(result);
                 Method setter = getSetter(invocation.getMethod());
                 if (setter != null) {
                     setter.invoke(invocation.getThis(), result);
@@ -145,16 +167,17 @@ public class PropertyChangeListenerMixin extends DelegatingIntroductionIntercept
             return super.invoke(invocation);
         }
     }
-    
+
     /**
-     * @param setter    the setter method
-     * @return          the corresponding getter method
+     * @param setter
+     *            the setter method
+     * @return the corresponding getter method
      */
     private Method getGetter(Method setter) {
         Method getter = null;
 
         // attempt cache retrieval.
-        getter = (Method) methodCache.get(setter);
+        getter = (Method) m_methodCache.get(setter);
 
         if (getter != null) {
             return getter;
@@ -165,8 +188,8 @@ public class PropertyChangeListenerMixin extends DelegatingIntroductionIntercept
             getter = setter.getDeclaringClass().getMethod(getterName);
 
             // cache getter
-            synchronized (methodCache) {
-                methodCache.put(setter, getter);
+            synchronized (m_methodCache) {
+                m_methodCache.put(setter, getter);
             }
         } catch (NoSuchMethodException ex) {
             // must be write only
@@ -174,16 +197,17 @@ public class PropertyChangeListenerMixin extends DelegatingIntroductionIntercept
         }
         return getter;
     }
-    
+
     /**
-     * @param getter    the getter method
-     * @return          the corresponding setter method
+     * @param getter
+     *            the getter method
+     * @return the corresponding setter method
      */
     private Method getSetter(Method getter) {
         Method setter = null;
 
         // attempt cache retrieval.
-        setter = (Method) methodCache.get(getter);
+        setter = (Method) m_methodCache.get(getter);
 
         if (setter != null) {
             return setter;
@@ -191,12 +215,12 @@ public class PropertyChangeListenerMixin extends DelegatingIntroductionIntercept
 
         String setterName = getter.getName().replaceFirst("get", "set");
         try {
-            setter = getter.getDeclaringClass().getMethod(
-                    setterName, getter.getReturnType());
+            setter = getter.getDeclaringClass().getMethod(setterName,
+                    getter.getReturnType());
 
             // cache setter
-            synchronized (methodCache) {
-                methodCache.put(getter, setter);
+            synchronized (m_methodCache) {
+                m_methodCache.put(getter, setter);
             }
         } catch (NoSuchMethodException ex) {
             // must be write only
@@ -204,8 +228,8 @@ public class PropertyChangeListenerMixin extends DelegatingIntroductionIntercept
         }
         return setter;
     }
-    
+
     public ClassValidator getClassValidator() {
-        return classValidator;
+        return m_classValidator;
     }
 }
