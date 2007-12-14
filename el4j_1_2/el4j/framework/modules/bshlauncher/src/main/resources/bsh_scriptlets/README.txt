be00ed851364930a4bec52e84acb97021b5dcd22
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2007-12-14 16:20:50 +0000

    tag at wrong place

--- el4j_1_2/el4j/framework/modules/bshlauncher/src/main/resources/bsh_scriptlets/README.txt
+++
@@ -1,6 +0,0 @@
-/**
-
-This directory holds files that can be launched by bsh. 
-They are automatically in the classpath of the bsh Interpreter.
-
-*/
--- el4j_1_2/el4j/framework/tests/remoting/versioning/src/main/java/ch/elca/el4j/tests/remoting/service/package-info.java
+++
@@ -1,6 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(
-///    namespace = "classpath:/ch.elca.el4j.tests.remoting.service",
-    namespace = "another.test.another.path",
-    elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED
-)
-package ch.elca.el4j.tests.remoting.service;
