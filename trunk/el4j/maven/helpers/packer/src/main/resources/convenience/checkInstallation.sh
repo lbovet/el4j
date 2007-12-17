e79c7e256fe744a1518c045016b30ccef2d67409
poser55 <poser55@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2007-12-17 16:00:04 +0000

    completed the scripts

--- trunk/el4j/maven/helpers/packer/src/main/resources/convenience/checkInstallation.sh
+++ trunk/el4j/maven/helpers/packer/src/main/resources/convenience/checkInstallation.sh
@@ -22,4 +22,4 @@ echo Must print out the version number of a Java 5 JDK or newer.
 echo Must print out the same version number as above.
 echo Must print out the version "2.0.7" or newer.
 echo Must print something like -Xmx1024M -Xss128k -XX:MaxPermSize=512M -Duser.language=en -Duser.region=US
-. 
+ 
