4aabbb413735c59e6625e5a6f3b62e749d73842d
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2007-12-17 09:04:47 +0000

    release scripts added

---
+++ el4j/etc/release-scripts/finalize.sh
@@ -0,0 +1,93 @@
+#!/bin/bash -e
+
+function update() {
+	awk -F= '\
+    {if ($1 ~ /-SNAPSHOT$/) {
+        sub (/-SNAPSHOT$/, "", $1)
+        major = substr($1 , 0, length($1) - 1)
+        minor = substr($1 , length($1))
+        minor++
+        print major minor "-SNAPSHOT"
+    } else
+        print}' $1 > $1.new
+    mv $1.new $1
+}
+
+# make sure you are in right folder
+if ! [ -e external ] ; then
+	echo "Error: Folder 'external' not found. Go to its parent folder (el4j)!"
+	exit
+fi
+
+cd external
+svn switch https://el4j.svn.sourceforge.net/svnroot/el4j/trunk/el4j
+
+echo "Process internal? (y/n)"
+read performInternal
+
+if [ $performInternal == "y" ] ; then
+	cd ../internal
+	svn switch https://cvs.elca.ch/subversion/el4j-internal/trunk
+	cd ..
+fi
+
+
+echo "Enter current el4j version number (without -SNAPSHOT)"
+read el4jCurrent
+echo "Enter next el4j snapshot version number (without -SNAPSHOT)"
+read el4jNext
+
+el4jCurrent=$el4jCurrent-SNAPSHOT
+el4jNext=$el4jNext-SNAPSHOT
+
+# list pom files
+find ./ -name "pom.xml" > pom.files.txt
+find ./ -name "site.xml" >> pom.files.txt
+
+#####################################
+# el4j a.b-SNAPSHOT -> x.y-SNAPSHOT #
+#####################################
+for i in $(cat pom.files.txt) ; do
+	if [ $(grep -c "<version>$el4jCurrent</version>" $i) -eq 1 ] ; then
+		cat $i | sed "s/$el4jCurrent/$el4jNext/" > $i.new
+		mv $i.new $i
+		echo "    OK $i"
+	else
+		echo "CHECK! $i"
+	fi
+done
+
+################################
+# el4j modules: increase minor #
+################################
+
+update external/pom.xml
+
+if [ $performInternal == "y" ] ; then
+	update internal/pom.xml
+fi
+
+
+echo ""
+echo "#####################################################################################"
+echo "#Work is not finished yet! Check all pom.xml and site.xml files if they are correct #"
+echo "#Look at the files that have a CHECK! tag.                                          #"
+echo "#####################################################################################"
+echo ""
+
+rm pom.files.txt
+
+echo "Hint: The next step is to commit the new version to the trunk."
+echo "... and don't forget to restore your maven settings (mv ~/.m2/settings.xml.backup ~/.m2/settings.xml)"
+
+exit
+# not used yet
+
+echo "Press Enter to clean up (restore settings.xml, delete el4jFresh)"
+read dummy
+
+# restore settings
+mv ~/.m2/settings.xml.backup ~/.m2/settings.xml
+
+cd d:
+rm -R el4jFresh
---
+++ el4j/etc/release-scripts/release.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -e
+
+# make sure you are in right folder
+if ! [ -e external ] ; then
+	echo "Error: Folder 'external' not found. Go to its parent folder (el4j)!"
+	exit
+fi
+
+echo "The script will sometimes ask you if something is correct."
+echo "If it is, press Enter, if it is not, press Ctrl-C."
+echo "Press Enter to continue"
+read dummy
+
+function makeRelease() {
+	echo "Step 1: Release $1"
+	cd $1
+
+	echo "Executing svn update..."
+	svn update
+	echo "Which revision was the latest release candidate?"
+	read revision
+	echo "Revision is $revision, OK?"
+	read dummy
+
+	echo "Which tag should be chosen? (like 1.1.1)"
+	read tagDot
+	tagScore=$(echo $tagDot | sed "s/\./_/g")
+	echo "Tag is $tagDot, OK?"
+	read dummy
+
+	echo "Command is:"
+	echo svn copy $2/trunk $2/tags/el4j_$tagScore -r $revision -m "Tagging the EL4J release $tagDot"
+	echo "Continue?"
+	read dummy
+
+	svn copy $2/trunk $2/tags/el4j_$tagScore -r $revision -m "Tagging the EL4J release $tagDot"
+
+	svn switch $2/tags/el4j_$tagScore$3 .
+	svn revert -R .
+	svn update
+
+	echo "$2 done."
+	cd ..
+}
+
+makeRelease "external" "https://el4j.svn.sourceforge.net/svnroot/el4j" "/el4j"
+
+echo "Process internal? (y/n)"
+read performInternal
+
+if [ $performInternal != "y" ] ; then
+	exit
+fi
+
+makeRelease "internal" "https://cvs.elca.ch/subversion/el4j-internal" ""
+echo "Hint: The next script to execute is deploy.sh"
