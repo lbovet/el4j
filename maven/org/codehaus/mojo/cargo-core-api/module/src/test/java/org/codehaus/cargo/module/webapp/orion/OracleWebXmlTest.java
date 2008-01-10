efe9f1be5fd6f279c4668dcd5b4a058b73e02188
chumard <chumard@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-01-10 09:28:04 +0000

    Import customized org.codehaus.cargo:cargo-core-api-* artifacts

---
+++ maven/org/codehaus/mojo/cargo-core-api/module/src/test/java/org/codehaus/cargo/module/webapp/orion/OracleWebXmlTest.java
@@ -0,0 +1,57 @@
+/*
+ * ========================================================================
+ *
+ * Copyright 2005 Vincent Massol.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================================================================
+ */
+package org.codehaus.cargo.module.webapp.orion;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+import org.codehaus.cargo.module.AbstractDocumentBuilderTest;
+import org.jdom.Element;
+import org.codehaus.cargo.module.webapp.EjbRef;
+
+/**
+ * Unit tests for {@link OracleWebXml}.
+ *
+ * @version $Id: OracleWebXmlTest.java 1476 2007-05-29 09:35:24Z magnayn $
+ */
+public class OracleWebXmlTest extends AbstractDocumentBuilderTest
+{
+    /**
+     * Tests that a ejb reference description can be added.
+     *
+     * @throws Exception If an unexpected error occurs
+     */
+    public void testAddEjbReferenceDescription() throws Exception
+    {
+        String xml = "<orion-web-app></orion-web-app>";
+        
+        OrionWebXml descr = OrionWebXmlIo.parseOrionXml(new ByteArrayInputStream(xml.getBytes()));        
+        EjbRef ref = new EjbRef();
+        ref.setName("foo");
+        ref.setJndiName("fee");
+        descr.addEjbReference(ref);
+
+        List nl = descr.getDocument().getRootElement().getChildren("ejb-ref-mapping");
+        Element n = (Element)nl.get(0);
+        assertEquals("foo", n.getAttribute("name").getValue());
+        assertEquals("fee", n.getAttribute("location").getValue());
+        assertEquals(1, nl.size());
+    }
+
+}
