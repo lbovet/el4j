f1a50e8406e3c174f5d755e7c211e7b7461e7e99
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-02-08 11:23:58 +0000

    i18n-able error page added, cleanup

--- trunk/el4j/framework/modules/jsf/jar/src/main/java/ch/elca/el4j/seam/generic/errorhandling/ErrorResolver.java
+++ trunk/el4j/framework/modules/jsf/jar/src/main/java/ch/elca/el4j/seam/generic/errorhandling/ErrorResolver.java
@@ -17,23 +17,11 @@
 
 package ch.elca.el4j.seam.generic.errorhandling;
 
-import java.io.IOException;
 import java.io.Serializable;
-
-import javax.faces.application.ViewExpiredException;
-import javax.servlet.ServletException;
-
-import org.hibernate.HibernateException;
-import org.hibernate.JDBCException;
-import org.hibernate.SessionException;
-
-import org.hibernate.StaleObjectStateException;
 
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-
-import com.sun.facelets.tag.TagException;
 
 /**
  * Seam component for generic error handling. This class is used by the custom
@@ -46,20 +34,18 @@ import com.sun.facelets.tag.TagException;
  * $", "$Author$" ); </script>
  * 
  * @author Frank Bitzer (FBI)
+ * @author Stefan Wismer (SWI)
  */
 @Name("errorResolver")
 @Scope(ScopeType.CONVERSATION)
 public class ErrorResolver implements Serializable {
 
     /**
-     * Returns a funny text describing the root cause of specified exception.
+     * Returns the simple name of the root cause of the specified exception.
+     * This is used in error.xhtml and error_??.properties
      * 
-     * Only implemented for some kinds of Exceptions.
-     * 
-     * TODO: use internationalised messages...
-     * 
-     * @param ex
-     * @return
+     * @param exception    the exception
+     * @return             the simple name
      */
     public String getErrorMessage(Exception exception) {
 
@@ -69,69 +55,7 @@ public class ErrorResolver implements Serializable {
             ex = ex.getCause();
         }
 
-        String result = "Unknown problem.";
-
-        if (ex.getClass().equals(IOException.class)) {
-            
-            result = "An input/output operation failed.";
-        }
-
-        if (ex.getClass().equals(NullPointerException.class)) {
-
-            result = "An internal object could not be found or created.";
-        }
-
-        if (ex.getClass().equals(AssertionError.class)) {
-
-            result = "An internal assertion was violenced.";
-        }
-
-        if (ex.getClass().equals(HibernateException.class)) {
-
-            result = "An internal database error occured.";
-        }
-
-        if (ex.getClass().equals(JDBCException.class)) {
-
-            result = "An error in a database query was detected.";
-
-        }
-
-        if (ex.getClass().equals(ViewExpiredException.class)) {
-
-            result = "Your view expired. Please reload this page.";
-
-        }
-
-        if (ex.getClass().equals(SessionException.class)) {
-
-            result = "Your database session expired. "
-                    + "Please reload this page.";
-
-        }
-
-        if (ex.getClass().equals(ServletException.class)) {
-
-            result = "Your request could not be processed due to "
-                    + "an internal servlet error.";
-
-        }
-        if (ex.getClass().equals(TagException.class)) {
-
-            result = "Your request could not be processed due to "
-                    + "an unknown or wrong tag in this page.";
-
-        }
-
-        if (ex.getClass().equals(StaleObjectStateException.class)) {
-
-            result = "You tried to edit or delete an object"
-                    + ", but its state has changed in meantime.";
-
-        }
-
-        return result;
-
+        return ex.getClass().getSimpleName();
     }
 
 }
