38a9da32d653a6d18c8950cca65ab2861bbd96b6
poser55 <poser55@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-02-22 13:49:24 +0000

    updated doc, added news, added troubleshooting guide

--- trunk/el4j/site/src/site/resources/docs/pdf/wikiPdfDownloadScript.sh
+++ trunk/el4j/site/src/site/resources/docs/pdf/wikiPdfDownloadScript.sh
@@ -4,6 +4,7 @@
 
 wget -v -O  ReferenceDoc.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/ReferenceDoc?skin=pdf
 wget -v -O  FrequentlyAskedQuestions.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/FrequentlyAskedQuestions?skin=pdf
+wget -v -O  TroubleshootingGuide.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/TroubleshootingMavenAndEl4j?skin=pdf
 wget -v -O  GettingStarted.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/GettingStarted?skin=pdf
 wget -v -O  GettingStartedDeveloper.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/GettingStartedDeveloper?skin=pdf
 wget -v -O  UniqueEL4JFeatures.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/FeaturesOfEl4j?skin=pdf
