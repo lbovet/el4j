11b36fa9612121a1ea4c08c642b38a2b06e6b37c
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-02-22 16:00:38 +0000

    little improvement in scripts, new script for updating pdf documentation

---
+++ el4j/etc/release-scripts/updateDoc.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo "Since we are going to modify files we have to switch to the trunk."
+echo "Press any key to continue or Ctrl-C to stop."
+read dummy
+
+cd external
+echo "Executing: svn switch https://el4j.svn.sourceforge.net/svnroot/el4j/trunk/el4j"
+svn switch https://el4j.svn.sourceforge.net/svnroot/el4j/trunk/el4j
+cd ..
+
+version=$(cat external/pom.xml | grep "<version.el4j-framework.current>" -A 1 | tail -n 1 | tr -d ' \r\n')
+
+cd external/site/src/site/resources/docs/pdf
+./wikiPdfDownloadScript.sh
+
+svn status
+problems=$(svn status | grep "^?" | wc -l)
+if [ $problems -ne 0 ] ; then
+	echo "Attention: new files were added. Commit has to be done manually"
+	echo "Snippet: svn commit -m \"Update documentation for release of EL4J $version\""
+else
+	echo "Executing: svn commit -m \"Update documentation for release of EL4J $version\""
+	echo "Press any key to continue or Ctrl-C to stop."
+	read dummy
+	svn commit -m "Update documentation for release of EL4J $version"
+fi
+
+echo ""
+echo "Keep in mind that external now points to the trunk!"
