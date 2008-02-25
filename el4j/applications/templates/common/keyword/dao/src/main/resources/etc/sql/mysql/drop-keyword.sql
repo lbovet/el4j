31c894ec0cd52de7ba07574b2e35b3e7dfcd0fce
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-02-25 07:55:37 +0000

    drop keywords also has to drop all references (due to drop problems using oracle)

--- el4j/applications/templates/common/keyword/dao/src/main/resources/etc/sql/mysql/drop-keyword.sql
+++ el4j/applications/templates/common/keyword/dao/src/main/resources/etc/sql/mysql/drop-keyword.sql
@@ -4,3 +4,11 @@
 -- Author: $Author: enylaios $
 
 DROP TABLE keywords;
+
+DROP TABLE files;
+DROP TABLE annotations;
+DROP TABLE referenceKeywordRelationships;
+DROP TABLE books;
+DROP TABLE formalpublications;
+DROP TABLE links;
+DROP TABLE referencesTable;
