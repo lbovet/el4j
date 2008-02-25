39cf4aaff615b53c774332726d3055a06e1c02b4
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-02-25 08:31:22 +0000

    jsf demo now uses keywords again, keywords know runs on LEAF3 (oracle) (undo changes in revision 2708 and 2717)

--- trunk/el4j/applications/templates/common/keyword/dao/src/main/resources/etc/sql/oracle/drop-keyword.sql
+++ trunk/el4j/applications/templates/common/keyword/dao/src/main/resources/etc/sql/oracle/drop-keyword.sql
@@ -5,15 +5,3 @@
 
 DROP TABLE keywords;
 DROP SEQUENCE keyword_sequence;
-
-DROP TABLE files;
-DROP TABLE annotations;
-DROP TABLE referenceKeywordRelationships;
-DROP TABLE books;
-DROP TABLE formalpublications;
-DROP TABLE links;
-DROP TABLE referencesTable;
-
-DROP SEQUENCE reference_sequence;
-DROP SEQUENCE annotation_sequence;
-DROP SEQUENCE file_sequence;
