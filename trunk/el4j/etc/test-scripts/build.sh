9d943effed3a263013dab4c8ed6ff32b90fc2fdd
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-02-28 09:21:19 +0000

    template tests added, cosmetic cleanups

--- trunk/el4j/etc/test-scripts/build.sh
+++ trunk/el4j/etc/test-scripts/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -e
 
 # This build script needs the following three environment variables:
-# PROFILE :      the profile specified in execProfile.sh
+# PROFILE :      the profile specified in run.sh
 # LOG_DIR :      the directory to put the surefire reports
 #
-# - current directory must contain subfolders external and internal, settings.xml and run.sh (use downloadScripts.sh)
-# - parent folder must contain passwords.sh
+# - current directory must contain subfolders external and/or internal, settings.xml and run.sh (use downloadScripts.sh)
 
 PROFILE=$1
 LOG_DIR=$2
@@ -15,6 +14,6 @@ mkdir $LOG_DIR
 
 cp settings.xml ~/.m2/settings.xml 
 
-# perform the maven build and copy the result in any case
-( ./run.sh $PROFILE ) || ( ( \
+# perform the maven build and copy the result in case of errors
+./run.sh $PROFILE || ( ( \
 find . -path "*target/surefire-reports*" -exec cp  {} $LOG_DIR/ \; ) && (( ps -ely )) && (( 0 )) )
