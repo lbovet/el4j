70f96a25cc284a9aae0202b968f68cfed2612162
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-03-05 14:09:28 +0000

    optional third parameter added to change jrockit version.

--- trunk/el4j/etc/test-scripts/build.sh
+++ trunk/el4j/etc/test-scripts/build.sh
@@ -12,7 +12,13 @@ LOG_DIR=$2
 # Log directory
 mkdir $LOG_DIR
 
-cp settings.xml ~/.m2/settings.xml 
+if [ $# -ge 3 ] && [ $3 == "Java6" ] ; then
+	cat settings.xml \
+		| sed 's#jrockit-R27.4.0-jdk1.5.0_12#jrockit-R27.5.0-jre1.6.0_03#' \
+		> ~/.m2/settings.xml
+else
+	cp settings.xml ~/.m2/settings.xml 
+fi
 
 # perform the maven build and copy the result in case of errors
 ./run.sh $PROFILE || ( ( \
