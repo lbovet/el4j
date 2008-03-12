2c7809eb6ddc282a350a7ec9b9638ee8bd78473a
dbdelca <dbdelca@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-03-12 08:08:12 +0000

    Fixed a bug where encryption properties would not load from a .jar. Some code cleanup and beautification.

--- el4j/framework/modules/env/src/main/java/ch/elca/el4j/util/env/PropertyEncryptionUtil.java
+++ el4j/framework/modules/env/src/main/java/ch/elca/el4j/util/env/PropertyEncryptionUtil.java
@@ -16,169 +16,228 @@
  */
 package ch.elca.el4j.util.env;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.util.Properties;
 
-import ch.elca.el4j.util.encryption.AbstractPropertyEncryptor;
-import ch.elca.el4j.util.encryption.PasswordSource;
-
 import org.apache.commons.logging.Log;
-import ch.elca.el4j.core.context.ModuleApplicationContext;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 
+import ch.elca.el4j.core.context.ModuleApplicationContext;
+import ch.elca.el4j.util.encryption.AbstractPropertyEncryptor;
+import ch.elca.el4j.util.encryption.PasswordSource;
+
 /**
  * Utility to load a custom key from file (cryptor.properties). This is in a
  * separate class to make it accessible from Override- and
- * Placeholder-configurer and avoid code duplication.
- *
- * <script type="text/javascript">printFileStatus
- *   ("$URL: https://el4j.svn.sourceforge.net/svnroot/el4j/trunk/el4j/etc/eclipse/codeTemplates.xml $",
- *    "$Revision: 2754 $",
- *    "$Date: 2008-03-04 09:04:15 +0100 (Tue, 04 Mar 2008) $",
- *    "$Author: swismer $"
- * );</script>
- *
-* @author David Bernhard (DBD)
+ * Placeholder-configurer and avoid code duplication. <script
+ * type="text/javascript">printFileStatus ("$URL:
+ * https://el4j.svn.sourceforge.net/svnroot/el4j/trunk/el4j/etc/eclipse/codeTemplates.xml
+ * $", "$Revision: 2754 $", "$Date: 2008-03-04 09:04:15 +0100 (Tue, 04 Mar 2008)
+ * $", "$Author: swismer $" );</script>
+ * 
+ * @author David Bernhard (DBD)
  */
 public class PropertyEncryptionUtil {
 
-	private static Log s_logger = LogFactory
-			.getLog(ModuleApplicationContext.EL4J_DEBUGGING_LOGGER);
+    /**
+     * The logger.
+     */
+    private static Log s_logger = LogFactory
+        .getLog(ModuleApplicationContext.EL4J_DEBUGGING_LOGGER);
+    
+    /**
+     * The default cryptor configuration file.  
+     */
+    protected String m_defaultConfigFile = "classpath:cryptor.properties";
+ 
+    /**
+     * This is used to see if the internal cryptor class is available.
+     */
+    private ClassLoader m_cl;
 
-	private ClassLoader m_cl;
+    /**
+     * The classloader returns type Class which we save and cast later on.
+     */
+    @SuppressWarnings("unchecked")
+    private Class m_c;
 
-	@SuppressWarnings("unchecked")
-	private Class m_c;
+        /**
+     * Status of the encryption module. We need this because
+     * we cannot initialize in a constructor
+     * but must wait for spring to set a property in the caller. 
+     * Therefore an init() method is provided.
+     */
+    private enum CryptStatus {
+        
+        /** We have not called init() yet to check if we are in internal. */
+        UNINITED, 
+        
+        /** init() found no cryptor. */
+        EXTERNAL, 
+        
+        /** init found cryptor and all is ready. */
+        ACTIVE, 
+        
+        /** Turned off by deactivate(). */
+        DEACTIVATED
+    };
+    
+    /**
+     * Stores the current status.
+     */
+    private CryptStatus m_status;
 
-	/**
-	 * UNINITED - We have not called init() yet to check if we are in internal.
-	 * EXTERNAL - init() found no cryptor. ACTIVE - init found cryptor and all
-	 * is ready. DEACTIVATED - Turned off by deactivate().
-	 */
-	private enum CryptStatus {
-		UNINITED, EXTERNAL, ACTIVE, DEACTIVATED
-	};
+    /**
+     * The cryptor object which provides encryption.
+     */
+    private AbstractPropertyEncryptor m_cryptor;
 
-	private CryptStatus m_status;
+    /**
+     * An optional source for a true custom password.
+     */
+    private PasswordSource m_source;
 
-	protected String m_defaultConfigFile = "classpath:cryptor.properties";
+    /**
+     * Flag to indicate whether m_source is used.
+     */
+    private boolean m_useSource = false;
 
-	private AbstractPropertyEncryptor m_cryptor;
+    /**
+     * Constructor. We cannot do init() here due to the caller not having all 
+     * properties at construction time.
+     */
+    public PropertyEncryptionUtil() {
+        m_status = CryptStatus.UNINITED;
+    }
 
-	private PasswordSource m_source;
+    /**
+     * @return Whether init() has been called. 
+     */
+    public boolean isInited() {
+        return !m_status.equals(CryptStatus.UNINITED);
+    }
 
-	private boolean m_useSource = false;
+    /**
+     * @return Whether we can do encryption.
+     */
+    public boolean isActive() {
+        return m_status.equals(CryptStatus.ACTIVE);
+    }
 
-	public PropertyEncryptionUtil() {
-		m_status = CryptStatus.UNINITED;
-	}
+    /**
+     * Deactivate encryption features.
+     */
+    public void deactivate() {
+        m_status = CryptStatus.DEACTIVATED;
+    }
 
-	public boolean isInited() {
-		return !m_status.equals(CryptStatus.UNINITED);
-	}
+    /**
+     * Set up encryption. We need to pass ctx to access resources.
+     * @param ctx ModuleApplicationContext
+     */
+    public void init(ApplicationContext ctx) {
+        init(ctx, m_defaultConfigFile);
+    }
 
-	public boolean isActive() {
-		return m_status.equals(CryptStatus.ACTIVE);
-	}
+    /**
+     * @param ctx ModuleApplicationContext
+     * @param configFile Custom config file location.
+     */
+    public void init(ApplicationContext ctx, String configFile) {
 
-	public void deactivate() {
-		m_status = CryptStatus.DEACTIVATED;
-	}
+        if (m_status != CryptStatus.UNINITED) {
+            throw new RuntimeException(
+                "You cannot call init() more than once.");
+        }
 
-	public void init(ApplicationContext ctx) {
-		init(ctx, m_defaultConfigFile);
-	}
+        /* Check whether we are in internal. */
+        m_cl = Thread.currentThread().getContextClassLoader();
+        try {
+            m_c = m_cl
+                .loadClass("ch.elca.el4j.modules.encryption.PropertyEncryptor");
+            m_cryptor = (AbstractPropertyEncryptor) m_c.newInstance();
+        } catch (ClassNotFoundException e) {
+            // An exception landing us here means we are in external.
+            m_status = CryptStatus.EXTERNAL;
+            return;
+        } catch (Exception e) {
+            throw new RuntimeException("Error initializing cryptor.");
+        }
 
-	public void init(ApplicationContext ctx, String configFile) {
+        /*
+         * We have a cryptor - set it up. Read password to use from
+         * configuration file.
+         */
 
-		if (m_status != CryptStatus.UNINITED) {
-			throw new RuntimeException("You cannot call init() more than once.");
-		}
+        m_status = CryptStatus.ACTIVE;
 
-		/* Check whether we are in internal. */
-		m_cl = Thread.currentThread().getContextClassLoader();
-		try {
-			m_c = m_cl
-					.loadClass("ch.elca.el4j.modules.encryption.PropertyEncryptor");
-			m_cryptor = (AbstractPropertyEncryptor) m_c.newInstance();
-		} catch (ClassNotFoundException e) {
-			// An exception landing us here means we are in external.
-			m_status = CryptStatus.EXTERNAL;
-			return;
-		} catch (Exception e) {
-			throw new RuntimeException("Error initializing cryptor.");
-		}
+        s_logger.info("Trying to read cryptor config file: " + configFile);
 
-		/*
-		 * We have a cryptor - set it up. Read password to use from
-		 * configuration file.
-		 */
+        Properties p = new Properties();
+        Resource res = ctx.getResource(configFile);
+        if (!res.exists()) {
+            s_logger
+                .error("The config file " + configFile + " does not exist.");
+        }
 
-		m_status = CryptStatus.ACTIVE;
+        try {
+            //File file = res.getFile();
+            p.load(res.getInputStream());
+        } catch (Exception e) {
+            s_logger.error("Config file " + configFile + " is not accessible.");
+            e.printStackTrace();
+        }
 
-		s_logger.info("Trying to read cryptor config file: " + configFile);
+        if (p.containsKey("cryptor.passwordSource")
+            && p.containsKey("cryptor.customPassword")) {
+            String source = p.getProperty("cryptor.passwordSource");
+            String custom = p.getProperty("cryptor.customPassword");
 
-		Properties p = new Properties();
-		Resource res = ctx.getResource(configFile);
-		if (!res.exists()) {
-			s_logger
-					.error("The config file " + configFile + " does not exist.");
-		}
+            try {
+                if (source.equals("mixed")) {
+                    String key = m_cryptor.decrypt(custom);
+                    m_cryptor.deriveKey(key);
+                } else if (source.equals("custom")) {
+                    if (m_useSource) {
+                        m_cryptor.deriveKey(this.m_source.getPassword());
+                    } else {
+                        // If no source defined but using a custom password,
+                        // error.
+                        s_logger.error("Internal password mode set to custom"
+                            + "but no passwordSource defined.");
+                    }
+                }
+                s_logger.info("Success reading file.");
+            } catch (Exception e) {
+                s_logger.error("Error reading config file.");
+            }
+        } else {
+            s_logger.error("Config file must contain entries "
+                + "cryptor.passwordSource and " + "cryptor.customPassword.");
+        }
+    }
 
-		try {
-			File file = res.getFile();
-			p.load(new FileInputStream(file));
-		} catch (Exception e) {
-			s_logger.error("Config file " + configFile + " is not accessible.");
-		}
+    /**
+     * Called to get the cryptor object managed by this class.
+     * @return An initialized cryptor.
+     */
+    public AbstractPropertyEncryptor getCryptor() {
+        if (!isActive()) {
+            throw new RuntimeException("You can only get a cryptor in the "
+                + "ACTIVE state.");
+        }
+        return m_cryptor;
+    }
 
-		if (p.containsKey("cryptor.passwordSource")
-				&& p.containsKey("cryptor.customPassword")) {
-			String source = p.getProperty("cryptor.passwordSource");
-			String custom = p.getProperty("cryptor.customPassword");
-
-			try {
-				if (source.equals("mixed")) {
-					String key = m_cryptor.decrypt(custom);
-					m_cryptor.deriveKey(key);
-				} else if (source.equals("custom")) {
-					if (m_useSource) {
-						m_cryptor.deriveKey(this.m_source.getPassword());
-					} else {
-						// If no source defined but using a custom password,
-						// error.
-						s_logger.error("Internal password mode set to custom"
-								+ "but no passwordSource defined.");
-					}
-				}
-				s_logger.info("Success reading file.");
-			} catch (Exception e) {
-				s_logger.error("Error reading config file.");
-			}
-		} else {
-			s_logger
-					.error("Config file must contain entries "
-							+ "cryptor.passwordSource and "
-							+ "cryptor.customPassword.");
-		}
-	}
-
-	public AbstractPropertyEncryptor getCryptor() {
-		if (!isActive()) {
-			throw new RuntimeException("You can only get a cryptor in the "
-					+ "ACTIVE state.");
-		}
-		return m_cryptor;
-	}
-
-	public void setSource(PasswordSource source) {
-		// As we might not be init()ed yet, just save the source -
-		// and set a flag.
-		this.m_source = source;
-		m_useSource = true;
-	}
+    /**
+     * @param source A custom PasswordSource
+     */
+    public void setSource(PasswordSource source) {
+        // As we might not be init()ed yet, just save the source -
+        // and set a flag.
+        this.m_source = source;
+        m_useSource = true;
+    }
 
 }
