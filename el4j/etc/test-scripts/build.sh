d723bb71997caa92affc884fd26b51e4b3eb64b2
dbdelca <dbdelca@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-04-23 09:24:34 +0000

    Undo of last change - logging works, but hudson filters the tags.

--- el4j/etc/test-scripts/build.sh
+++ el4j/etc/test-scripts/build.sh
@@ -16,10 +16,10 @@ if [ $# -ge 3 ] && [ $3 == "Java6" ] ; then
 	cat settings.xml \
 		| sed 's#jrockit-R27.4.0-jdk1.5.0_12#jrockit-R27.5.0-jdk1.6.0_03#' \
 		> ~/.m2/settings.xml
-	OPTS="-Del4j.java.version=1.6 -Dplexus.logger.type=html"
+	OPTS="-Del4j.java.version=1.6"
 else
 	cp settings.xml ~/.m2/settings.xml
-	OPTS="-Dplexus.logger.type=html"
+	OPTS=""
 fi
 
 # perform the maven build and copy the result in case of errors
