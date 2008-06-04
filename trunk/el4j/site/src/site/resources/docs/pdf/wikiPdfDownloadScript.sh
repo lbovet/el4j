33113ef8e9d8f9e6b1e1789376da4acb710e1b27
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-06-04 14:33:58 +0000

    documentation updated (plus changes due to reorganized wiki)

--- trunk/el4j/site/src/site/resources/docs/pdf/wikiPdfDownloadScript.sh
+++ trunk/el4j/site/src/site/resources/docs/pdf/wikiPdfDownloadScript.sh
@@ -4,9 +4,9 @@
 
 wget -v -O  ReferenceDoc.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/ReferenceDoc?skin=pdf
 wget -v -O  FrequentlyAskedQuestions.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/FrequentlyAskedQuestions?skin=pdf
-wget -v -O  TroubleshootingGuide.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/TroubleshootingMavenAndEl4j?skin=pdf
+wget -v -O  TroubleshootingGuide.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/TroubleshootingGuideEl4j?skin=pdf
 wget -v -O  GettingStarted.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/GettingStarted?skin=pdf
-wget -v -O  GettingStartedDeveloper.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/GettingStartedDeveloper?skin=pdf
+wget -v -O  SetupEL4J.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/SetupEL4J?skin=pdf
 wget -v -O  UniqueEL4JFeatures.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/FeaturesOfEl4j?skin=pdf
 # do this better by hand: 
 # wget -v -O  MavenCheatSheet_EL4J.pdf http://wiki.elca.ch/twiki/el4j/bin/view/EL4J/MavenCheatSheet?skin=pdf
