11fb7f8ba73a46fe376368c0cba4bb08317a3d52
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-07-22 06:34:15 +0000

    Abstract base class for tests added to module-core (keyword and refdb use it now)

---
+++ el4j/framework/modules/core/src/test/java/ch/elca/el4j/tests/core/ModuleApplicationContextCache.java
@@ -0,0 +1,172 @@
+/*
+ * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
+ * the spring framework, http://el4j.sf.net
+ * Copyright (C) 2005 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * EL4J is published under the GNU Lesser General Public License (LGPL)
+ * Version 2.1. See http://www.gnu.org/licenses/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+package ch.elca.el4j.tests.core;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import ch.elca.el4j.core.context.ModuleApplicationContext;
+
+/**
+ * This class implements a cache for {@link ModuleApplicationContext}s.
+ *
+ * <script type="text/javascript">printFileStatus
+ *   ("$URL$",
+ *    "$Revision$",
+ *    "$Date$",
+ *    "$Author$"
+ * );</script>
+ *
+ * @author Stefan Wismer (SWI)
+ */
+public final class ModuleApplicationContextCache {
+	/**
+	 * The actual cache.
+	 */
+	private static Map<Configuration, ConfigurableApplicationContext> s_cache;
+	
+	
+	/**
+	 * The hidden constructor.
+	 */
+	private ModuleApplicationContextCache() {
+		throw new AssertionError();
+	}
+	
+	/**
+	 * @param config    a {@link ModuleApplicationContext} configuration
+	 * @return          the corresponding {@link ModuleApplicationContext}
+	 */
+	public static ApplicationContext get(ModuleApplicationContextConfiguration config) {
+		return get(new Configuration(config));
+	}
+	
+	/**
+	 * @param inclusiveConfigLocations         the string array with inclusive locations
+	 * @param exclusiveConfigLocations         the string array with exclusive locations
+	 * @param allowBeanDefinitionOverriding    <code>true</code> if bean definition overriding should be allowed
+	 * @return                                 the corresponding {@link ModuleApplicationContext}
+	 */
+	public static ApplicationContext get(String[] inclusiveConfigLocations,
+			String[] exclusiveConfigLocations, boolean allowBeanDefinitionOverriding) {
+		
+		return get(new Configuration(inclusiveConfigLocations, exclusiveConfigLocations,
+			allowBeanDefinitionOverriding));
+	}
+	
+	/**
+	 * @param config    a {@link ModuleApplicationContext} configuration
+	 * @return          the corresponding {@link ModuleApplicationContext}
+	 */
+	private static synchronized ApplicationContext get(Configuration config) {
+		if (s_cache == null) {
+			s_cache = new HashMap<Configuration, ConfigurableApplicationContext>();
+		}
+		if (s_cache.containsKey(config)) {
+			ConfigurableApplicationContext loadedContext = s_cache.get(config);
+			// refresh context if necessary
+			if (!loadedContext.isActive()) {
+				loadedContext.refresh();
+			}
+			return loadedContext;
+		} else {
+			ConfigurableApplicationContext newContext = new ModuleApplicationContext(
+				config.getInclusiveConfigLocations(), config.getExclusiveConfigLocations(),
+				config.isBeanOverridingAllowed(), (ApplicationContext) null);
+			
+			s_cache.put(config, newContext);
+			return newContext;
+		}
+	}
+	
+	/**
+	 * Data holder for {@link ModuleApplicationContext} configurations that can be used as key in {@link Map}s.
+	 */
+	private static class Configuration implements ModuleApplicationContextConfiguration {
+		private final String[] m_inclusiveConfigLocations;
+		private final String[] m_exclusiveConfigLocations;
+		private final boolean m_allowBeanDefinitionOverriding;
+		
+		Configuration(String[] inclusiveConfigLocations, String[] exclusiveConfigLocations,
+			boolean allowBeanDefinitionOverriding) {
+			
+			m_inclusiveConfigLocations = inclusiveConfigLocations;
+			m_exclusiveConfigLocations = exclusiveConfigLocations;
+			m_allowBeanDefinitionOverriding = allowBeanDefinitionOverriding;
+		}
+		
+		Configuration(ModuleApplicationContextConfiguration config) {
+			m_inclusiveConfigLocations = config.getInclusiveConfigLocations();
+			m_exclusiveConfigLocations = config.getExclusiveConfigLocations();
+			m_allowBeanDefinitionOverriding = config.isBeanOverridingAllowed();
+		}
+		
+		/** {@inheritDoc} */
+		public String[] getInclusiveConfigLocations() {
+			return m_inclusiveConfigLocations;
+		}
+		
+		/** {@inheritDoc} */
+		public String[] getExclusiveConfigLocations() {
+			return m_exclusiveConfigLocations;
+		}
+		
+		/** {@inheritDoc} */
+		public boolean isBeanOverridingAllowed() {
+			return m_allowBeanDefinitionOverriding;
+		}
+
+		/** {@inheritDoc} */
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result
+				+ (m_allowBeanDefinitionOverriding ? 1231 : 1237);
+			result = prime * result
+				+ Arrays.hashCode(m_exclusiveConfigLocations);
+			result = prime * result
+				+ Arrays.hashCode(m_inclusiveConfigLocations);
+			return result;
+		}
+
+		/** {@inheritDoc} */
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			final Configuration other = (Configuration) obj;
+			if (m_allowBeanDefinitionOverriding != other.m_allowBeanDefinitionOverriding)
+				return false;
+			if (!Arrays.equals(m_exclusiveConfigLocations,
+				other.m_exclusiveConfigLocations))
+				return false;
+			if (!Arrays.equals(m_inclusiveConfigLocations,
+				other.m_inclusiveConfigLocations))
+				return false;
+			return true;
+		}
+	}
+}
