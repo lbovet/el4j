3cf1712866fae3fcf865856ee214db46e8c4bd3a
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-10-17 08:40:26 +0000

    bat and sh files updated

--- el4j/maven/helpers/packer/src/main/resources/convenience/finishInstallation.sh
+++ el4j/maven/helpers/packer/src/main/resources/convenience/finishInstallation.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo
-echo This is a convenience setup. In case something does not work as expected,
-echo please check the documentation under http://wiki.elca.ch/twiki/el4j/bin/view/EL4J 
-echo and http://el4j.sourceforge.net/
+echo "This is a convenience setup. In case something does not work as expected,"
+echo "please check the documentation under http://wiki.elca.ch/twiki/el4j/bin/view/EL4J"
+echo "and http://el4j.sourceforge.net/"
 echo
 
 if [ ! -e ~/.bash_profile ] ; then
@@ -37,8 +37,8 @@ esac
 
 echo
 echo
-echo do the following steps if within elca:
-echo  \* uncomment the proxy tag in settings.xml
-echo  \* uncomment the mirror tags in settings.xml
+echo "do the following steps if within elca:"
+echo " * uncomment the proxy tag in settings.xml"
+echo " * uncomment the mirror tags in settings.xml"
 echo
 
