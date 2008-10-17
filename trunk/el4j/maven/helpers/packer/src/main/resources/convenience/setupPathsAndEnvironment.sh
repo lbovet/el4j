3a0091eaee0977d3634375a2e1dde52d8cfc29df
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2008-10-17 08:40:26 +0000

    bat and sh files updated

--- trunk/el4j/maven/helpers/packer/src/main/resources/convenience/setupPathsAndEnvironment.sh
+++ trunk/el4j/maven/helpers/packer/src/main/resources/convenience/setupPathsAndEnvironment.sh
@@ -3,6 +3,7 @@
 #### setup each time:
 alias debugmaven='export MAVEN_OPTS="-Xmx1024M -Xss128k -XX:MaxPermSize=512M -Duser.language=en -Duser.region=US -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -Dcom.sun.management.jmxremote"'
 alias runmaven='export MAVEN_OPTS="-Xmx1024M -Xss128k -XX:MaxPermSize=512M -Duser.language=en -Duser.region=US -Dcom.sun.management.jmxremote"'
+export CATALINA_OPTS="-Xmx1024M -Xss128k -XX:MaxPermSize=512M"
 
 case "`uname`" in
 CYGWIN*)
