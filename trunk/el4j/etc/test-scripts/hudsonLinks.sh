9d6069afa52875280fabb9d1d841f84a69678dcb
niggli <niggli@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2009-01-06 11:05:48 +0000

    small script repairing the symlinks for hudson

---
+++ trunk/el4j/etc/test-scripts/hudsonLinks.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# After updates, Hudson resets the folder structure in ~/.hudson/jobs/*/workspace:
+# the symbolic links are deleted and standard folders are created.
+# This script deletes this folders and recreates the symbolic links
+# The script has to be executed in the folder ~/.hudson/jobs/
+
+JOB=EL4J-EXTERNAL-CodeCheck
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent/ $JOB/workspace/frequent
+
+JOB=EL4J-EXTERNAL_archetypes
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent/ $JOB/workspace/frequent
+
+JOB=EL4J-EXTERNAL_archetypes_Java6
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent6/ $JOB/workspace/frequent
+
+JOB=EL4J-EXTERNAL_nightly_build
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent/ $JOB/workspace/frequent
+
+JOB=EL4J-EXTERNAL_nightly_build_Java6
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent6/ $JOB/workspace/frequent
+
+JOB=EL4J-EXTERNAL_svn_build
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent/ $JOB/workspace/frequent
+
+JOB=EL4J-EXTERNAL_svn_build_Java6
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent6/ $JOB/workspace/frequent
+
+JOB=EL4J-INTERNAL-CodeCheck
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent/ $JOB/workspace/frequent
+
+JOB=EL4J-INTERNAL_nightly_build
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent/ $JOB/workspace/frequent
+
+JOB=EL4J-INTERNAL_nightly_build_Java6
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent6/ $JOB/workspace/frequent
+
+JOB=EL4J-INTERNAL_svn_build
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent/ $JOB/workspace/frequent
+
+JOB=EL4J-INTERNAL_svn_build_Java6
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent6/ $JOB/workspace/frequent
+
+JOB=EL4J-WEEKLY_build
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent/ $JOB/workspace/frequent
+
+JOB=EL4J-WEEKLY_build_Java6
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent6/ $JOB/workspace/frequent
+
+JOB=ELCA-ServiceIntegration_CodeCheck
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent/ $JOB/workspace/frequent
+
+JOB=ELCA-ServiceIntegration_svn_build
+rm -rf $JOB/workspace/frequent
+ln -s ~/el4j/hudson_build_workspaces/frequent/ $JOB/workspace/frequent
+
