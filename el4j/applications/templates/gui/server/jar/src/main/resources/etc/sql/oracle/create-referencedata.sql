2d89d662551edd5de5149f3183d05c9ed66a079a
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2009-01-09 12:35:41 +0000

    refdb example data: porting changes in db2 SQL script to oracle

--- el4j/applications/templates/gui/server/jar/src/main/resources/etc/sql/oracle/create-referencedata.sql
+++ el4j/applications/templates/gui/server/jar/src/main/resources/etc/sql/oracle/create-referencedata.sql
@@ -1,5 +1,10 @@
 INSERT INTO REFERENCESTABLE (KEYID,NAME,HASHVALUE,DESCRIPTION,VERSION,INCOMPLETE,WHENINSERTED,DOCUMENTDATE,OPTIMISTICLOCKINGVERSION) VALUES (reference_sequence.nextVal,'The Complete Log4j Manual','lkjh','This book shows you how to use Log4J.','1.0',0,{ts '2008-08-13 14:43:38.692'},null,0);
 INSERT INTO BOOKS (KEYTOREFERENCE,AUTHORNAME,PUBLISHER,PAGENUM,ISBNNUMBER) VALUES (reference_sequence.currVal,'Ceki Gulcu','QOS.ch',206,'2970036908');
+INSERT INTO KEYWORDS (KEYID,NAME,DESCRIPTION,OPTIMISTICLOCKINGVERSION) VALUES (keyword_sequence.nextVal,'Java','Java books',0);
+INSERT INTO REFERENCEKEYWORDRELATIONSHIPS (KEYREFERENCE,KEYKEYWORD) VALUES (reference_sequence.currVal,keyword_sequence.currVal);
+INSERT INTO KEYWORDS (KEYID,NAME,DESCRIPTION,OPTIMISTICLOCKINGVERSION) VALUES (keyword_sequence.nextVal,'Web','Web development',0);
+INSERT INTO REFERENCEKEYWORDRELATIONSHIPS (KEYREFERENCE,KEYKEYWORD) VALUES (reference_sequence.currVal,keyword_sequence.currVal);
+INSERT INTO ANNOTATIONS (KEYID,KEYTOREFERENCE,ANNOTATOR,GRADE,CONTENT,WHENINSERTED,OPTIMISTICLOCKINGVERSION) VALUES (annotation_sequence.nextVal,reference_sequence.currVal,'pos',10,'this is a good book',{ts '2008-12-23 17:48:39.818'},0);
 
 INSERT INTO REFERENCESTABLE (KEYID,NAME,HASHVALUE,DESCRIPTION,VERSION,INCOMPLETE,WHENINSERTED,DOCUMENTDATE,OPTIMISTICLOCKINGVERSION) VALUES (reference_sequence.nextVal,'The LaTeX Companion',null,'Provides expert advice on using Latex''s basic formatting tools for creating all types of publications. Includes in-depth coverage of important extension packages for tabular and technical typesetting.',null,0,{ts '2008-08-13 14:46:44.628'},null,0);
 INSERT INTO BOOKS (KEYTOREFERENCE,AUTHORNAME,PUBLISHER,PAGENUM,ISBNNUMBER) VALUES (reference_sequence.currVal,'Mittelbach Frank, Goosens Michel',null,0,'0-201-36299-6');
