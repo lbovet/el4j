d1c11105d327c710056c1f2e99359ed1dfc8e4ee
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2009-04-09 09:46:38 +0000

    make EL4J package names consistent: module-offliner-tests

--- trunk/el4j/framework/tests/offliner/oracle/src/test/java/ch/elca/el4j/tests/persistence/hibernate/offlining/OracleTypedImpl.java
+++ trunk/el4j/framework/tests/offliner/oracle/src/test/java/ch/elca/el4j/tests/persistence/hibernate/offlining/OracleTypedImpl.java
@@ -14,7 +14,7 @@
  *
  * For alternative licensing, please contact info@elca.ch
  */
-package ch.elca.el4j.services.persistence.hibernate.offlining.test;
+package ch.elca.el4j.tests.persistence.hibernate.offlining;
 
 import java.io.Serializable;
 
