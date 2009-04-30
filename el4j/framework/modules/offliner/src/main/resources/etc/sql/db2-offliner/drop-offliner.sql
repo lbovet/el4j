ea838f3e6159730fed26a0fbcc41cecfc6349dd2
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2009-04-30 14:27:50 +0000

    offliner: use sqlFindReplacePattern of maven-database-plugin to generate offline tables

--- el4j/framework/modules/offliner/src/main/resources/etc/sql/db2-offliner/drop-offliner.sql
+++ el4j/framework/modules/offliner/src/main/resources/etc/sql/db2-offliner/drop-offliner.sql
@@ -1,11 +1,6 @@
 -- Create script for local schema.
 -- This file is read and executed by the test starter.
 
--- USE LOCAL;
-
-DROP TABLE SIMPLEPERSON;
-DROP TABLE PERSON;
-
 -- The offliner tables.
 
 DROP TABLE KEYMAP;
