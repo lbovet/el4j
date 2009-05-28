7de87ccd33c754d7f0b08a7fa2ca78fa6d2543c6
arruud <arruud@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2009-05-28 08:41:55 +0000

    Updated site script to support Java6 option.

--- trunk/el4j/site/site.sh
+++ trunk/el4j/site/site.sh
@@ -10,26 +10,26 @@ mvn clean
 cd ..
 
 # install root pom
-mvn clean install -N
+mvn clean install -N $1
 
 # install framework pom
 cd framework
-mvn clean install -N
+mvn clean install -N $1
 
 # generate doc for modules
-mvn -N -Pgenerate.doc.set.framework-modules collector:aggregate-files
-mvn -N -Pgenerate.doc.set.framework-modules jxr:jxr &
-mvn -Pgenerate.doc.set.framework-modules javadoc:javadoc &
+mvn -N -Pgenerate.doc.set.framework-modules collector:aggregate-files $1
+mvn -N -Pgenerate.doc.set.framework-modules jxr:jxr $1 &
+mvn -Pgenerate.doc.set.framework-modules javadoc:javadoc $1 &
 
 # generate doc for applications
-mvn -N -Pgenerate.doc.set.framework-applications collector:aggregate-files
-mvn -N -Pgenerate.doc.set.framework-applications jxr:jxr &
-mvn -Pgenerate.doc.set.framework-applications javadoc:javadoc &
+mvn -N -Pgenerate.doc.set.framework-applications collector:aggregate-files $1
+mvn -N -Pgenerate.doc.set.framework-applications jxr:jxr $1 &
+mvn -Pgenerate.doc.set.framework-applications javadoc:javadoc $1 &
 
 # generate doc for tests
-mvn -N -Pgenerate.doc.set.framework-tests collector:aggregate-files
-mvn -N -Pgenerate.doc.set.framework-tests jxr:jxr &
-mvn -Pgenerate.doc.set.framework-tests javadoc:test-javadoc &
+mvn -N -Pgenerate.doc.set.framework-tests collector:aggregate-files $1
+mvn -N -Pgenerate.doc.set.framework-tests jxr:jxr $1 &
+mvn -Pgenerate.doc.set.framework-tests javadoc:test-javadoc $1 &
 
 # generate doc for demos
 #mvn -N -Pgenerate.doc.set.framework-demos collector:aggregate-files
@@ -43,37 +43,37 @@ cd ..
 
 # generate doc for plugins
 cd maven/plugins
-mvn clean install
-mvn site
+mvn clean install $1
+mvn site $1
 plugins=$(find -maxdepth 1 -name "maven-*-plugin"  -exec basename {} \;)
 cd ../..
 
 cd framework/plugins
-mvn clean install
-mvn site
+mvn clean install $1
+mvn site $1
 frameworkPlugins=$(find -maxdepth 1 -name "maven-*-plugin"  -exec basename {} \;)
 cd ../..
 
 # generate test reports (no -Ptest.quiet)
-mvn install -Pauto,tomcat6x,db2
-mvn antrun:run -f site/pom.xml -Pcopy.surefire-report.tomcat-derby
+mvn install -Pauto,tomcat6x,db2 $1
+mvn antrun:run -f site/pom.xml -Pcopy.surefire-report.tomcat-derby $1
 
-mvn install -Pauto,weblogic10x,oracle
-mvn antrun:run -f site/pom.xml -Pcopy.surefire-report.weblogic-oracle
+mvn install -Pauto,weblogic10x,oracle $1
+mvn antrun:run -f site/pom.xml -Pcopy.surefire-report.weblogic-oracle $1
 
 # aggregate files
 cd site
-mvn collector:aggregate-files -Pcollect-doc
+mvn collector:aggregate-files -Pcollect-doc $1
 for p in $plugins ; do
-	mvn collector:aggregate-files -Pcollect-plugin-doc -Dplugin=$p
+	mvn collector:aggregate-files -Pcollect-plugin-doc -Dplugin=$p $1
 done
 for p in $frameworkPlugins ; do
-	mvn collector:aggregate-files -Pcollect-framework-plugin-doc -Dplugin=$p
+	mvn collector:aggregate-files -Pcollect-framework-plugin-doc -Dplugin=$p $1
 done
 
 # make site
-mvn site -Psurefire-report.tomcat-derby
-mvn site -Psurefire-report.weblogic-oracle
+mvn site -Psurefire-report.tomcat-derby $1
+mvn site -Psurefire-report.weblogic-oracle $1
 
 # deploy site
-mvn site-deploy
+mvn site-deploy $1
