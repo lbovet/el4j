22c7b25cbb74524cb3423deb4d75b26102e3ac70
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2009-07-09 10:49:44 +0000

    AbstractSlf4jEnabledMojo added (an extension of AbstractMojo that correctly sets up SLF4J using the maven logging back-end)

---
+++ el4j/maven/extensions/slf4j-maven/src/main/java/ch/elca/el4j/maven/plugins/AbstractSlf4jEnabledMojo.java
@@ -0,0 +1,41 @@
+/*
+ * EL4J, the Extension Library for the J2EE, adds incremental enhancements to
+ * the spring framework, http://el4j.sf.net
+ * Copyright (C) 2009 by ELCA Informatique SA, Av. de la Harpe 22-24,
+ * 1000 Lausanne, Switzerland, http://www.elca.ch
+ *
+ * EL4J is published under the GNU Lesser General Public License (LGPL)
+ * Version 2.1. See http://www.gnu.org/licenses/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * For alternative licensing, please contact info@elca.ch
+ */
+package ch.elca.el4j.maven.plugins;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.logging.Log;
+import org.slf4j.impl.MavenLoggerFactory;
+
+/**
+ * An extension of {@link AbstractMojo} that correctly sets up SLF4J using the maven logging back-end.
+ *
+ * <script type="text/javascript">printFileStatus
+ *   ("$URL$",
+ *    "$Revision$",
+ *    "$Date$",
+ *    "$Author$"
+ * );</script>
+ *
+ * @author Stefan Wismer (SWI)
+ */
+public abstract class AbstractSlf4jEnabledMojo extends AbstractMojo {
+	/** {@inheritDoc} */
+	public void setLog(Log log) {
+		super.setLog(log);
+		MavenLoggerFactory.setLog(getLog());
+	}
+}
