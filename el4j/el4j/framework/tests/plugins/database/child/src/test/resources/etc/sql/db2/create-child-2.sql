a54a5c66aaf4a590504d4008f2669da3077c00c4
swrelca <swrelca@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2010-08-23 07:38:53 +0000

    Remove branch.

--- el4j/el4j/framework/tests/plugins/database/child/src/test/resources/etc/sql/db2/create-child-2.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE TESTCHILDTWO(
-  name    VARCHAR(64)    UNIQUE NOT NULL
-);
-
--- check dependencies
-SELECT * FROM TESTCHILDONE;
--- el4j/el4j/sandbox/old/framework/modules/offliner/src/main/resources/etc/sql/db2-offliner/drop-offliner.sql
+++
@@ -1,7 +0,0 @@
--- Create script for local schema.
--- This file is read and executed by the test starter.
-
--- The offliner tables.
-
-DROP TABLE KEYMAP;
-DROP TABLE OFFLINERPROPERTIES;
