899457dae644cc75c00adf852c0fb25cb8c3a098
pbwelca <pbwelca@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2010-11-15 15:24:35 +0000

    fix hudsonLinks.sh, add a readme

--- trunk/el4j/etc/test-scripts/hudsonLinks.sh
+++ trunk/el4j/etc/test-scripts/hudsonLinks.sh
@@ -4,17 +4,36 @@
 # This script deletes this folders and recreates the symbolic links
 # The script has to be executed in the folder ~/.hudson/jobs/
 
-JOBS5="EL4J-EXTERNAL-CodeCheck EL4J-EXTERNAL_archetypes EL4J-EXTERNAL_nightly_build EL4J-EXTERNAL_svn_build EL4J-INTERNAL-CodeCheck EL4J-INTERNAL_nightly_build EL4J-INTERNAL_svn_build ELCA-ServiceIntegration_CodeCheck ELCA-ServiceIntegration_svn_build"
-JOBS6="EL4J-EXTERNAL_archetypes_Java6 EL4J-EXTERNAL_nightly_build_Java6 EL4J-EXTERNAL_svn_build_Java6 EL4J-INTERNAL_nightly_build_Java6 EL4J-INTERNAL_svn_build_Java6"
-for JOB in $JOBS5 ; do
+JOBS6="
+EL4J-EXTERNAL_archetypes
+EL4J-EXTERNAL-CodeCheck
+EL4J-EXTERNAL_nightly_build
+EL4J-EXTERNAL_svn_build
+EL4J-GUI-tests
+EL4J-INTERNAL-CodeCheck
+EL4J-INTERNAL_nightly_build
+EL4J-INTERNAL_svn_build
+EL4J-SONAR_weekly_report
+EL4J-WEEKLY_build
+ELCA-ServiceIntegration_CodeCheck
+ELCA-ServiceIntegration_svn_build"
+JOBS7="
+EL4J-EXTERNAL_archetypes_Java7
+EL4J-EXTERNAL_nightly_build_Java7
+EL4J-EXTERNAL_svn_build_Java7
+EL4J-INTERNAL_nightly_build_Java7
+EL4J-INTERNAL_svn_build_Java7
+EL4J-WEEKLY_build_Java7"
+
+for JOB in $JOBS6 ; do
 	rm -rf $JOB/workspace/frequent
 	ln -s ~/el4j/hudson_build_workspaces/frequent/ $JOB/workspace/frequent
 	echo "Done for Job $JOB"
 done
 
-for JOB in $JOBS6 ; do
+for JOB in $JOBS7 ; do
 	rm -rf $JOB/workspace/frequent
-	ln -s ~/el4j/hudson_build_workspaces/frequent6/ $JOB/workspace/frequent
+	ln -s ~/el4j/hudson_build_workspaces/frequent7/ $JOB/workspace/frequent
 	echo "Done for Job $JOB"
 done
 
