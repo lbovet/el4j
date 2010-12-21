b09ace47598df609be879087cc859d09093b6ab6
swismer <swismer@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2010-12-21 10:08:51 +0000

    fix bug in updateSvnProps.sh (files.tmp must be in unix mode), cleanup

--- el4j/etc/release-scripts/updateSvnProps.sh
+++ el4j/etc/release-scripts/updateSvnProps.sh
@@ -11,7 +11,6 @@
 # inside the file are also reseted / deleted. 
 
 
-tags="URL Date Author Revision Id"
 
 #check for echoonly mode
 if [ "$1" == "echoonly" ] ; then
@@ -28,33 +27,11 @@ fi
 echo "Press any key to continue..."
 read dummy
 
-#function definition to run svn propset command
-#parameters (1) command to run, (2) echomode
-function runcommand() {
-	#check for echo only mode
-	if [ $2 == 1 ] ; then
-		echo "$1"
-	else
-		$1
-	fi
-}
-
-#function defintion to set the keywords for the configured tags
-#parameters (1) pathtofile, (2) echomode
-function svnsetkeywords() {
-	#check for echo only mode
-	if [ $2 == 1 ] ; then
-		echo "svn propset svn:keywords \"$tags\" $1"
-	else
-		svn propset svn:keywords "$tags" $1
-	fi
-}
-
 #function defintion to delete property values in files
-#parameters (1) pathtofile, (2) echomode
+#parameters (1) pathtofile
 function deltagsinfile() {
 	#check for echo only mode
-	if [ $2 == 1 ] ; then
+	if [ $echoonly == 1 ] ; then
 		echo "mv $1 $1.tmp"
 		echo "sed -e 's/\$Revision:[^\$]\+\$/\$Revision\$/g' -e 's/\$URL:[^\$]\+\$/\$URL\$/g' -e 's/\$Date:[^\$]\+\$/\$Date\$/g' -e 's/\$Author:[^\$]\+\$/\$Author\$/g' $1.tmp > $1"
 		echo "rm $1.tmp"
@@ -65,90 +42,49 @@ function deltagsinfile() {
 	fi
 }
 
-#function to check for given props
-#parameters (1) property to check, (2) path to file
-#returns 1 if a property value has been found, else 0
-function checkforprops() {
-	while read line
-	do
-		if [[ $line != "" ]] ; then
-			return 1
+#ensure that given property is set
+#parameters (1) property to check, (2) property value, (3) path to file
+function ensurePropSet() {
+	if ! svn propget "$1" "$3" | grep "$2" > /dev/null ; then
+		if [ $echoonly == 1 ] ; then
+			echo "svn propset $1 \"$2\" $3"
+		else
+			svn propset $1 "$2" $3
 		fi
-	done < <(svn propget $1 $2)
-	return 0
+	fi
 }
 
 echo "Fetching complete filelist from SVN. This can take a while..."
 svn ls -R > files.tmp
-echo "Filelist completet. Processing now..."
+dos2unix files.tmp
+echo "Filelist completed. Processing now..."
 
 for i in $(cat files.tmp | egrep "(\.java)|(\.sql)|(\.txt)|(\.vm)|(\.htm)|(\.apt)|(\.twiki)|(\.css)|(\.dtd)|(\.properties)") ; do
-	checkforprops "svn:keywords" $i
-	if [ $? == 0 ] ; then
-		svnsetkeywords $i $echoonly
-		deltagsinfile $i $echoonly
-	fi
-	checkforprops "svn:eol-style" $i
-	if [ $? == 0 ] ; then
-		runcommand "svn propset svn:eol-style native $i" $echoonly
-	fi
+	ensurePropSet "svn:keywords" "URL Date Author Revision Id" $i
+	ensurePropSet "svn:eol-style" "native" $i
 done
 for i in $(cat files.tmp | egrep "(\.xml)|(\.xsl)|(\.xsd)") ; do
-	checkforprops "svn:keywords" $i
-	if [ $? == 0 ] ; then
-		svnsetkeywords $i $echoonly
-		deltagsinfile $i $echoonly
-	fi
-	checkforprops "svn:eol-style" $i
-	if [ $? == 0 ] ; then
-		runcommand "svn propset svn:eol-style native $i" $echoonly
-	fi
-	checkforprops "svn:mime-type" $i
-	if [ $? == 0 ] ; then
-		runcommand "svn propset svn:mime-type text/xml $i" $echoonly
-	fi
+	ensurePropSet "svn:keywords" "URL Date Author Revision Id" $i
+	ensurePropSet "svn:eol-style" "native" $i
+	ensurePropSet "svn:mime-type" "text/xml" $i
 done
 for i in $(cat files.tmp | egrep "\.png") ; do
-	checkforprops "svn:mime-type" $i
-	if [ $? == 0 ] ; then
-		runcommand "svn propset svn:mime-type image/png $i" $echoonly
-	fi
+	ensurePropSet "svn:mime-type" "image/png" $i
 done
 for i in $(cat files.tmp | egrep "\.jpg") ; do
-	checkforprops "svn:mime-type" $i
-	if [ $? == 0 ] ; then
-		runcommand "svn propset svn:mime-type image/jpeg $i" $echoonly
-	fi
+	ensurePropSet "svn:mime-type" "image/jpeg" $i
 done
 for i in $(cat files.tmp | egrep "\.gif") ; do
-	checkforprops "svn:mime-type" $i
-	if [ $? == 0 ] ; then
-		runcommand "svn propset svn:mime-type image/gif $i" $echoonly
-	fi
+	ensurePropSet "svn:mime-type" "image/gif" $i
 done
 for i in $(cat files.tmp | egrep "\.pdf") ; do
-	checkforprops "svn:mime-type" $i
-	if [ $? == 0 ] ; then
-		runcommand "svn propset svn:mime-type application/pdf $i" $echoonly
-	fi
+	ensurePropSet "svn:mime-type" "application/pdf" $i
 done
 for i in $(cat files.tmp | egrep "\.sh") ; do
-	checkforprops "svn:eol-style" $i
-	if [ $? == 0 ] ; then
-		runcommand "svn propset svn:eol-style LF $i" $echoonly
-	fi
-	checkforprops "svn:keywords" $i
-	if [ $? == 0 ] ; then
-		svnsetkeywords $i $echoonly
-	fi
-	checkforprops "svn:mime-type" $i
-	if [ $? == 0 ] ; then
-		runcommand "svn propset svn:mime-type application/x-sh $i" $echoonly
-	fi
-	checkforprops "svn:executable" $i
-	if [ $? == 0 ] ; then
-		runcommand "svn propset svn:executable ON $i" $echoonly
-	fi
+	ensurePropSet "svn:eol-style" "LF" $i
+	ensurePropSet "svn:keywords" "URL Date Author Revision Id" $i
+	ensurePropSet "svn:mime-type" "application/x-sh" $i
+	ensurePropSet "svn:executable" "ON" $i
 done
 
 rm files.tmp
