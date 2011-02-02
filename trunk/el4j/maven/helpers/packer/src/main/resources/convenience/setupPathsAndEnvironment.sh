33b6694ac0985cd89a322ff649505a4b1f57fe0a
pbwelca <pbwelca@29f8d62c-c30d-0410-9bd4-d2982912d9c2>
2011-02-02 12:58:48 +0000

    increase perm gen space

--- trunk/el4j/maven/helpers/packer/src/main/resources/convenience/setupPathsAndEnvironment.sh
+++ trunk/el4j/maven/helpers/packer/src/main/resources/convenience/setupPathsAndEnvironment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh 
 
 #### setup each time:
-alias debugmaven='export MAVEN_OPTS="-ea -Xmx1024M -Xss128k -XX:MaxPermSize=256M -Duser.language=en -Duser.region=US -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -Dcom.sun.management.jmxremote -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC"'
-alias runmaven='export MAVEN_OPTS="-ea -Xmx1024M -Xss128k -XX:MaxPermSize=256M -Duser.language=en -Duser.region=US -Dcom.sun.management.jmxremote -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC"'
+alias debugmaven='export MAVEN_OPTS="-ea -Xmx1024M -Xss128k -XX:MaxPermSize=512M -Duser.language=en -Duser.region=US -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -Dcom.sun.management.jmxremote -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC"'
+alias runmaven='export MAVEN_OPTS="-ea -Xmx1024M -Xss128k -XX:MaxPermSize=512M -Duser.language=en -Duser.region=US -Dcom.sun.management.jmxremote -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC"'
 export CATALINA_OPTS="-ea -Xmx1024M -Xss128k -XX:MaxPermSize=256M"
 
 case "`uname`" in
